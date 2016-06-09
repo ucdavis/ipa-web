33bd16d4d6282461b574a0d3d24327d40b8a064c
Christopher Thielen <cmthielen@ucdavis.edu>
2016-06-08 18:53:50 -0700

    Confirm to AbstractAuthenticationToken as needed.

--- src/main/java/edu/ucdavis/dss/ipa/security/AuthenticationToken.java
+++ src/main/java/edu/ucdavis/dss/ipa/security/AuthenticationToken.java
@@ -1,24 +1,39 @@
 package edu.ucdavis.dss.ipa.security;
 
 import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.util.Collection;
 
 public class AuthenticationToken extends AbstractAuthenticationToken {
-    private AuthUserDetails userDetails;
+    private AuthenticationPrincipal userPrincipal;
 
-    public AuthenticationToken(AuthUserDetails userDetails) {
-        super(userDetails.getAuthorities());
-        super.setAuthenticated(true);
-
-        this.userDetails = userDetails;
-
-        setDetails(userDetails);
+    /**
+     * Creates a token with the supplied array of authorities.
+     *
+     * @param authorities the collection of <tt>GrantedAuthority</tt>s for the principal
+     *                    represented by this authentication object.
+     */
+    public AuthenticationToken(Collection<? extends GrantedAuthority> authorities) {
+        super(authorities);
     }
+
+
+//    public AuthenticationToken(AuthenticationPrincipal userPrincipal) {
+//        //super(userDetails.getAuthorities());
+//        super.setAuthenticated(true);
+//
+//        this.userPrincipal = userPrincipal;
+//
+//        setDetails(userDetails);
+//    }
 
     @Override
     public Object getCredentials() { return "NA"; }
 
     @Override
-    public Object getPrincipal() { return userDetails; }
+    public Object getPrincipal() { return userPrincipal; }
 
     //public AuthUserDetails getUserDetails() { return userDetails; }
 
@@ -27,7 +42,7 @@ public class AuthenticationToken extends AbstractAuthenticationToken {
     //public Long getUserId() { return userDetails.getId(); }
 
     @Override
-    public String getName() { return userDetails.getName(); }
+    public String getName() { return userPrincipal.toString(); }; //.Details.getName(); }
 
     @Override
     public String toString() { return getPrincipal().toString(); }
