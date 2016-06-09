bc1163ca51f282b9bc0ddeb7a233dc9a5a0c5b84
Obada Kadri <okadri@ucdavis.edu>
2016-06-09 16:36:17 -0700

    #2 Creates Locations table migration and drops Buildings

---
+++ src/main/resources/db/migration/V110__Create_Locations_Table.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `Locations` (
+  `LocationId` INT(11) NOT NULL,
+  `Description` VARCHAR(50) NOT NULL,
+  `Workgroups_WorkgroupId` INT(11) NOT NULL,
+  PRIMARY KEY (`LocationId`),
+  INDEX `fk_Location_Workgroups_idx` (`Workgroups_WorkgroupId` ASC),
+  CONSTRAINT `fk_Location_Workgroups`
+    FOREIGN KEY (`Workgroups_WorkgroupId`)
+    REFERENCES `Workgroups` (`WorkgroupId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+DROP TABLE `Buildings`;
+
+ALTER TABLE `Activities` 
+DROP COLUMN `Buildings_BuildingId`,
+CHANGE COLUMN `Room` `BannerLocation` VARCHAR(20) NULL DEFAULT NULL ;
+
+UPDATE `Activities`
+SET `BannerLocation` = "";
