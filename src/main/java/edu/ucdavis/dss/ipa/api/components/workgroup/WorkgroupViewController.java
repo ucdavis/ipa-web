25827313488f9e2ae26b223991a6ca4ccf9922bb
Obada Kadri <okadri@ucdavis.edu>
2016-06-10 13:07:56 -0700

    #3 Adds the addTag route

--- src/main/java/edu/ucdavis/dss/ipa/api/components/workgroup/WorkgroupViewController.java
+++ src/main/java/edu/ucdavis/dss/ipa/api/components/workgroup/WorkgroupViewController.java
@@ -2,6 +2,10 @@ package edu.ucdavis.dss.ipa.api.components.workgroup;
 
 import edu.ucdavis.dss.ipa.api.components.workgroup.views.WorkgroupView;
 import edu.ucdavis.dss.ipa.api.components.workgroup.views.factories.WorkgroupViewFactory;
+import edu.ucdavis.dss.ipa.entities.Track;
+import edu.ucdavis.dss.ipa.entities.Workgroup;
+import edu.ucdavis.dss.ipa.services.TrackService;
+import edu.ucdavis.dss.ipa.services.WorkgroupService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,14 +16,23 @@ import javax.servlet.http.HttpServletResponse;
 @CrossOrigin // TODO: make CORS more specific depending on profile
 public class WorkgroupViewController {
 
-	@Inject
-	WorkgroupViewFactory workgroupViewFactory;
+	@Inject WorkgroupViewFactory workgroupViewFactory;
+	@Inject TrackService trackService;
+	@Inject WorkgroupService workgroupService;
 
 	@PreAuthorize("hasPermission(#workgroupCode, 'workgroup', 'academicCoordinator')")
 	@RequestMapping(value = "/api/workgroupView/{workgroupCode}", method = RequestMethod.GET, produces="application/json")
 	@ResponseBody
 	public WorkgroupView getWorkgroupViewByCode(@PathVariable String workgroupCode, HttpServletResponse httpResponse) {
 		return workgroupViewFactory.createWorkgroupView(workgroupCode);
+	}
+
+	@PreAuthorize("hasPermission(#workgroupCode, 'workgroup', 'academicCoordinator')")
+	@RequestMapping(value = "/api/workgroupView/{workgroupCode}/tags", method = RequestMethod.POST, produces="application/json")
+	@ResponseBody
+	public Track addTag(@PathVariable String workgroupCode, @RequestBody Track tag, HttpServletResponse httpResponse) {
+		Workgroup workgroup = workgroupService.findOneByCode(workgroupCode);
+		return trackService.findOrCreateTrackByWorkgroupAndTrackName(workgroup, tag.getName());
 	}
 
 }
