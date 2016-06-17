06ffecfc8bcf11f04755ffe2bb5f15c42a4cbe4a
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2016-06-17 01:17:55 -0700

    #8 Renaming Courses table and foreign keys

---
+++ src/main/resources/db/migration/V118__Renaming_Courses_Tables_and_Foreign_Keys.sql
@@ -0,0 +1,13 @@
+ALTER TABLE `Tracks` RENAME TO  `Tags` ;
+ALTER TABLE `CourseOfferingGroups_has_Tracks` RENAME TO  `Courses_has_Tags` ;
+
+ALTER TABLE `Courses_has_Tags` CHANGE COLUMN `CourseOfferingGroups_id` `CourseId` INT(10) UNSIGNED NOT NULL ;
+
+ALTER TABLE `Courses_has_Tags` DROP FOREIGN KEY `fk_CourseOfferingGroups_has_Tracks_Tracks1`;
+ALTER TABLE `Courses_has_Tags` CHANGE COLUMN `Tracks_TrackId` `TrackId` INT(11) NOT NULL ,
+  DROP INDEX `fk_CourseOfferingGroups_has_Tracks_Tracks1` ,
+  ADD INDEX `fk_Courses_Tracks_idx` (`TrackId` ASC);
+ALTER TABLE `Courses_has_Tags` ADD CONSTRAINT `fk_Courses_has_Tracks`
+  FOREIGN KEY (`TrackId`) REFERENCES `tags` (`Id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION;
