569e95d1ad0cb702eec482a3bead8388bcd81d62
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2016-06-29 09:44:56 -0700

    Removing unneeded imports

--- src/main/java/edu/ucdavis/dss/ipa/api/components/auth/AuthController.java
+++ src/main/java/edu/ucdavis/dss/ipa/api/components/auth/AuthController.java
@@ -1,4 +1,4 @@
-package edu.ucdavis.dss.ipa.api;
+package edu.ucdavis.dss.ipa.api.components.auth;
 
 import java.util.*;
 
@@ -8,9 +8,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 import edu.ucdavis.dss.ipa.entities.UserRole;
-import edu.ucdavis.dss.ipa.security.Authorization;
 import edu.ucdavis.dss.ipa.services.UserRoleService;
-import edu.ucdavis.dss.ipa.services.UserService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.SignatureException;
 import org.springframework.http.HttpHeaders;
@@ -24,7 +22,8 @@ import io.jsonwebtoken.SignatureAlgorithm;
 @RestController
 public class AuthController {
 
-    @Inject UserRoleService userRoleService;
+    @Inject
+    UserRoleService userRoleService;
 
     /**
      * Returns successful JWT token if logged into CAS, else
@@ -36,18 +35,16 @@ public class AuthController {
      */
     @CrossOrigin // TODO: make CORS more specific depending on profile
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public LoginResponse validate(@RequestBody final LoginResponse loginResponse,  final HttpServletRequest request) throws ServletException {
-        LoginResponse response = new LoginResponse();
-
+    public SecurityDTO validate(@RequestBody SecurityDTO securityDTO, HttpServletRequest request) throws ServletException {
         Enumeration<String> headers = request.getHeaderNames();
         Cookie[] cookies = request.getCookies();
         String signingKey = System.getenv("ipa.jwt.signingkey");
 
         // Check if the token exists, else check for CAS
-        if (loginResponse.token != null) {
+        if (securityDTO.token != null) {
             try {
                 Claims claims = Jwts.parser().setSigningKey(signingKey)
-                        .parseClaimsJws(loginResponse.token).getBody();
+                        .parseClaimsJws(securityDTO.token).getBody();
 
                 // This should execute only if parsing claims above
                 // successfully executes, hence token is valid
@@ -55,7 +52,6 @@ public class AuthController {
                 // Not using isSigned() because this method only checks
                 // if the token is signed regardless of the signing key
                 // https://github.com/jwtk/jjwt/blob/master/src/main/java/io/jsonwebtoken/JwtParser.java#L246
-                response.token = loginResponse.token;
             } catch (final SignatureException e) {
                 throw new ServletException("Invalid token.");
             }
@@ -71,25 +67,25 @@ public class AuthController {
                 List<UserRole> userRoles = userRoleService.findByLoginId(request.getUserPrincipal().getName());
                 System.out.println("generating token and setting new expirationDate value:");
                 System.out.println(expirationDate.toString());
-                response.token = Jwts.builder().setSubject(request.getUserPrincipal().getName())
+                securityDTO.token = Jwts.builder().setSubject(request.getUserPrincipal().getName())
                         .claim("userRoles", userRoles)
                         .claim("loginId", request.getUserPrincipal().getName())
                         .claim("expirationDate", expirationDate)
                         .setIssuedAt(new Date())
                         .signWith(SignatureAlgorithm.HS256, signingKey).compact();
+                securityDTO.setUserRoles(userRoles);
             }
         }
 
-        return response;
+        return securityDTO;
     }
 
     /**
-     *
      * @param request
      * @return redirects to "Location"
      */
     @RequestMapping(value = "/post-login", method = RequestMethod.GET)
-    public ResponseEntity processCAS(final HttpServletRequest request,
+    public ResponseEntity processCAS(HttpServletRequest request,
                                      @RequestParam(value = "ref", required = false) String ref) {
         Enumeration<String> requestHeaders = request.getHeaderNames();
         Cookie[] cookies = request.getCookies();
@@ -97,16 +93,6 @@ public class AuthController {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Location", ref);
 
-        return new ResponseEntity<byte []>(null, headers, HttpStatus.FOUND);
-    }
-
-    private static class LoginResponse {
-        public String token;
-        final public String redirect = "https://cas.ucdavis.edu/cas/login?service=http://localhost:8080/post-login";
-
-        public LoginResponse() { };
-        public LoginResponse(final String token) {
-            this.token = token;
-        }
+        return new ResponseEntity<byte[]>(null, headers, HttpStatus.FOUND);
     }
 }
