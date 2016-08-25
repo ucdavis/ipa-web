78a2c79c94f1eb1c2f3cb871618219efccc5b7d4
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2016-08-25 14:05:30 -0700

    Disable termUpdateTask, unclear if still necessary and is currently throwing DW client errors

--- src/main/java/edu/ucdavis/dss/ipa/tasks/TermUpdateTask.java
+++ src/main/java/edu/ucdavis/dss/ipa/tasks/TermUpdateTask.java
@@ -18,41 +18,41 @@ import edu.ucdavis.dss.ipa.services.TermService;
 
 @Service
 public class TermUpdateTask {
-	private static final Logger log = LogManager.getLogger();
-
-	@Inject TermService termService;
-	@Inject DataWarehouseRepository dwRepository;
-
-	private static boolean runningTask = false; /* flag to avoid multiple concurrent tasks */
-
-	@Scheduled( fixedDelay = 86400000 ) // Every 24 hours
-	@Async
-	public void updateTerms() {
-		if(runningTask) return; // avoid multiple concurrent jobs
-
-		runningTask = true;
-
-		List<DwTerm> dwTerms = null;
-
-		try {
-			dwTerms = dwRepository.getAllTerms();
-		} catch (Exception e) {
-			ExceptionLogger.logAndMailException(this.getClass().getName(), e);
-		}
-
-		if(dwTerms != null) {
-			for(DwTerm dwTerm : dwTerms) {
-				Term term = this.termService.findOrCreateByTermCode(dwTerm.getCode());
-				term.setStartDate(dwTerm.getBeginDate());
-				term.setEndDate(dwTerm.getEndDate());
-				this.termService.save(term);
-			}
-		}
-
-		if (dwTerms != null) {
-			log.info("Finished updating terms from DW. A total of " + dwTerms.size() + " terms.");
-		}
-
-		runningTask = false;
-	}
+//	private static final Logger log = LogManager.getLogger();
+//
+//	@Inject TermService termService;
+//	@Inject DataWarehouseRepository dwRepository;
+//
+//	private static boolean runningTask = false; /* flag to avoid multiple concurrent tasks */
+//
+//	@Scheduled( fixedDelay = 86400000 ) // Every 24 hours
+//	@Async
+//	public void updateTerms() {
+//		if(runningTask) return; // avoid multiple concurrent jobs
+//
+//		runningTask = true;
+//
+//		List<DwTerm> dwTerms = null;
+//
+//		try {
+//			dwTerms = dwRepository.getAllTerms();
+//		} catch (Exception e) {
+//			ExceptionLogger.logAndMailException(this.getClass().getName(), e);
+//		}
+//
+//		if(dwTerms != null) {
+//			for(DwTerm dwTerm : dwTerms) {
+//				Term term = this.termService.findOrCreateByTermCode(dwTerm.getCode());
+//				term.setStartDate(dwTerm.getBeginDate());
+//				term.setEndDate(dwTerm.getEndDate());
+//				this.termService.save(term);
+//			}
+//		}
+//
+//		if (dwTerms != null) {
+//			log.info("Finished updating terms from DW. A total of " + dwTerms.size() + " terms.");
+//		}
+//
+//		runningTask = false;
+//	}
 }
