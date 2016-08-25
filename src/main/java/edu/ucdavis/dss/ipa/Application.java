7f6deda122aa177b0a0291325ee17c05a714233f
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2016-08-25 14:05:00 -0700

    Enable tasks to run in spring boot

--- src/main/java/edu/ucdavis/dss/ipa/Application.java
+++ src/main/java/edu/ucdavis/dss/ipa/Application.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class Application {
     @Bean
