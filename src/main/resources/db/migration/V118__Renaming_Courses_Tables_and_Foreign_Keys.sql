db646a9f68cc5b3cae5911579a96f5310928bf59
Christopher Thielen <cmthielen@ucdavis.edu>
2016-09-06 11:26:07 -0700

    Capitalize MySQL tables for Linux compatibility.

--- src/main/resources/db/migration/V118__Renaming_Courses_Tables_and_Foreign_Keys.sql
+++ src/main/resources/db/migration/V118__Renaming_Courses_Tables_and_Foreign_Keys.sql
@@ -8,6 +8,6 @@ ALTER TABLE `Courses_has_Tags` CHANGE COLUMN `Tracks_TrackId` `TrackId` INT(11) 
   DROP INDEX `fk_CourseOfferingGroups_has_Tracks_Tracks1` ,
   ADD INDEX `fk_Courses_Tracks_idx` (`TrackId` ASC);
 ALTER TABLE `Courses_has_Tags` ADD CONSTRAINT `fk_Courses_has_Tracks`
-  FOREIGN KEY (`TrackId`) REFERENCES `tags` (`Id`)
+  FOREIGN KEY (`TrackId`) REFERENCES `Tags` (`Id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION;
