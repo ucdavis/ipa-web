c8d7f391f6178bdb2ed9d4f74c3982de477f5905
Mark Diez <markediez@gmail.com>
2016-09-21 11:24:58 -0700

    Create ActivityLog entity

---
+++ src/main/java/edu/ucdavis/dss/ipa/entities/ActivityLog.java
@@ -0,0 +1,49 @@
+package edu.ucdavis.dss.ipa.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import edu.ucdavis.dss.ipa.entities.validation.ValidActivity;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "ActivityLog")
+@ValidActivity
+public class ActivityLog {
+    long activityLogId;
+    long userId;
+    Timestamp timestamp;
+    String message;
+    
+    @Id
+    @Column(name = "ActivityLogId", unique = true, nullable = false)
+    @JsonProperty
+    public long getId() { return this.activityLogId; }
+
+    public void setId(long id) { this.activityLogId = id; }
+
+    @ManyToOne
+    @Column(name = "UsersId")
+    @JoinColumn(name= "Id", nullable=false)
+    @JsonProperty
+    public long getUserId() { return this.userId; }
+
+    public void setUserId(long userId) { this.userId = userId; }
+
+    @Basic
+    @Column(name = "Timestamp", unique = false, nullable = false)
+    @JsonProperty
+    public Timestamp getTimestamp() { return this.timestamp; }
+
+    public void setTimestamp(Timestamp timestamp) { this.timestamp = timestamp; }
+
+    @Basic
+    @Column(name = "Message", unique = false, nullable = false)
+    @JsonProperty
+    public String getMessage() { return this.message; }
+
+    public void setMessage(String message) { this.message = message; }
+}
