2be6e09788ee92c5b5228051d7f7ab70b8a01c4c
Mark Diez <markediez@gmail.com>
2016-09-23 10:32:38 -0700

    Add ActivityLogTag entity

---
+++ src/main/java/edu/ucdavis/dss/ipa/entities/ActivityLogTag.java
@@ -0,0 +1,35 @@
+package edu.ucdavis.dss.ipa.entities;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ActivityLogTag")
+public class ActivityLogTag {
+    private long id;
+    private long activityLogId;
+    private String tag;
+
+    @Basic
+    @Column(name = "Id", unique = true, nullable = false)
+    @JsonProperty
+    public long getId() { return this.id; }
+
+    public void setId(long id) { this.id = id; }
+
+    @Basic
+    @Column(name = "ActivityLogId", unique = true, nullable = false)
+    @JsonProperty
+    public long getActivityLogId() { return this.activityLogId; }
+
+    public void setActivityLogId(long activityLogId) { this.activityLogId = activityLogId; }
+
+    @Basic
+    @Column(name = "Tag", unique = true, nullable = false)
+    @JsonProperty
+    public String getTag() { return this.tag; }
+
+    public void setTag(String tag) { this.tag = tag; }
+
+}
