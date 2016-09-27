92164e834497e27ed720b28de5bc2f5aa735b544
Mark Diez <markediez@gmail.com>
2016-09-27 12:29:40 -0700

    Removed factories from ActivityLog component and add comments to its API call

--- src/main/java/edu/ucdavis/dss/ipa/api/components/activitylog/views/factories/ActivityLogViewFactory.java
+++
@@ -1,8 +0,0 @@
-package edu.ucdavis.dss.ipa.api.components.activitylog.views.factories;
-
-
-import edu.ucdavis.dss.ipa.api.components.activitylog.views.ActivityLogView;
-
-public interface ActivityLogViewFactory {
-    ActivityLogView createActivityLogView(String tag, long limit);
-}
