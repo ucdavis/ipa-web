4468abf8e30314d6175f4ed1c36d74e89b4c5332
Mark Diez <markediez@gmail.com>
2016-09-28 12:26:39 -0700

    Add debug log when adding an activity log tag

--- src/main/java/edu/ucdavis/dss/ipa/services/jpa/JpaActivityLogTagService.java
+++ src/main/java/edu/ucdavis/dss/ipa/services/jpa/JpaActivityLogTagService.java
@@ -4,6 +4,8 @@ import edu.ucdavis.dss.ipa.entities.ActivityLog;
 import edu.ucdavis.dss.ipa.entities.ActivityLogTag;
 import edu.ucdavis.dss.ipa.repositories.ActivityLogTagRepository;
 import edu.ucdavis.dss.ipa.services.ActivityLogTagService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
@@ -13,6 +15,8 @@ import java.util.List;
 
 @Service
 public class JpaActivityLogTagService implements ActivityLogTagService {
+    private static final Logger log = LoggerFactory.getLogger("edu.ucdavis.ipa");
+
     @Inject ActivityLogTagRepository activityLogTagRepository;
 
     @Override
@@ -34,19 +38,23 @@ public class JpaActivityLogTagService implements ActivityLogTagService {
     public void addActivityLogTag(ActivityLog activityLog, Object referenceEntity) {
         ActivityLogTag logTag = new ActivityLogTag();
         logTag.setActivityLog(activityLog);
-        String loggableTag = "";
+
+        // Initialize tag with the name of the entity
+        String loggableTag = referenceEntity.getClass().getSimpleName().toLowerCase();
 
         try {
-            String entityClass = referenceEntity.getClass().getSimpleName().toLowerCase();
             Method getEntityId = referenceEntity.getClass().getMethod("getId", null);
             long entityId = (long) getEntityId.invoke(referenceEntity, null);
 
-            loggableTag = entityClass + "_" + entityId;
+            loggableTag += "_" + entityId;
         } catch (InvocationTargetException e) {
+            log.debug("Invocation Target Exception when trying to get method getId on " + loggableTag);
             e.printStackTrace();
         } catch (IllegalAccessException e) {
+            log.debug("Illegal Access Exception when trying to get method getId on " + loggableTag);
             e.printStackTrace();
         } catch (NoSuchMethodException e) {
+            log.debug("Could not find method getId for " + loggableTag);
             e.printStackTrace();
         }
 
