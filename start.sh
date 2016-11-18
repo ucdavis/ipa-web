d3f464b79d5eb05ea3338e04b27c871a570cd547
Christopher Thielen <cmthielen@ucdavis.edu>
2016-11-18 12:52:40 -0800

    Use start.sh script to wait for MySQL to begin before running Spring Boot.

---
+++ start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Ensure MySQL is ready -- it may not be when the container first starts up
+# Solution credit: http://stackoverflow.com/questions/25503412/how-do-i-know-when-my-docker-mysql-container-is-up-and-mysql-is-ready-for-taking
+while ! mysqladmin ping -h "db" --silent; do
+    sleep 1
+done
+
+java -Djava.security.egd=file:/dev/./urandom \
+                    -Djava.security.egd=file:/dev/./urandom \
+                    -Dipa.datasource.url=${DATASOURCE_URL} \
+                    -Dipa.datasource.username=${DATASOURCE_USERNAME} \
+                    -Dipa.datasource.password=${DATASOURCE_PASSWORD} \
+                    -Dipa.spring.profile=${SPRING_PROFILE} \
+                    -Dipa.jwt.signingkey=${JWT_SIGNING_KEY} \
+                    -Dipa.logging.level=${LOGGING_LEVEL} \
+                    -Dipa.url.api=${BACKEND_URL} \
+                    -Dipa.url.frontend=${FRONTEND_URL} \
+                    -Djava.security.egd=file:/dev/./urandom \
+                    -Ddw.url=${DATAWAREHOUSE_URL} \
+                    -Ddw.token=${DATAWAREHOUSE_TOKEN} \
+                    -Ddw.port=${DATAWAREHOUSE_PORT} \
+                    -Dipa.email.protocol=${EMAIL_PROTOCOL} \
+                    -Dipa.email.auth=${EMAIL_AUTH} \
+                    -Dipa.email.debug=${EMAIL_DEBUG} \
+                    -Dipa.email.host=${EMAIL_HOST} \
+                    -Dipa.email.port=${EMAIL_PORT} \
+                    -Dipa.email.from=${EMAIL_FROM} \
+                    -jar /app.jar
