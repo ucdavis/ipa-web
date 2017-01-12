f90aaa33cd9b7ca899493ae54f8db7da24f1e236
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-01-12 08:46:38 -0800

    Filling out remaining metadata for schedule summary report excel download

--- src/main/java/edu/ucdavis/dss/ipa/api/components/scheduleSummaryReport/views/ScheduleSummaryReportExcelView.java
+++ src/main/java/edu/ucdavis/dss/ipa/api/components/scheduleSummaryReport/views/ScheduleSummaryReportExcelView.java
@@ -32,25 +32,83 @@ public class ScheduleSummaryReportExcelView extends AbstractXlsView {
         setExcelHeader(sheet);
 
         int row = 1;
+
         for(Course course : scheduleSummaryReportViewDTO.getCourses()) {
+
+            // Set course column
+            int col = 0;
+
             Row excelHeader = sheet.createRow(row);
+            excelHeader.createCell(col).setCellValue(course.getShortDescription());
 
-            excelHeader.createCell(0).setCellValue(course.getShortDescription());
-            List<String> tagNameList = course.getTags().stream().map(Tag::getName).collect(Collectors.toList());
-            excelHeader.createCell(1).setCellValue(StringUtils.join(tagNameList, ','));
+            for (SectionGroup sectionGroup : course.getSectionGroups()) {
 
-            int col = 2;
- //           for(Term term : scheduleSummaryReportViewDTO.getTerms()) {
-//                SectionGroup sectionGroup = this.getSectionGroupByCourseAndTermCode(course, term.getTermCode());
+                // Set Assigned column
+                col = 1;
 
- //               if (sectionGroup != null) {
- //                   excelHeader.createCell(col).setCellValue(sectionGroup.getPlannedSeats());
- //               }
+                for (TeachingAssignment teachingAssignment : sectionGroup.getTeachingAssignments()) {
+                    if (teachingAssignment.isApproved()) {
+                        excelHeader.createCell(col).setCellValue(teachingAssignment.getInstructor().getFullName());
+                        break;
+                    }
+                }
 
-  //              col++;
-  //          }
+                for (Section section : sectionGroup.getSections()) {
 
- //           row++;
+                    // Set Sequence Pattern
+                    col = 2;
+                    excelHeader.createCell(col).setCellValue(section.getSequenceNumber());
+
+                    // Set CRN
+                    col = 3;
+                    excelHeader.createCell(col).setCellValue(section.getCrn());
+
+                    // Set Seats
+                    col = 4;
+                    if (section.getSeats() != null) {
+                        excelHeader.createCell(col).setCellValue(section.getSeats());
+                    }
+
+                    for (Activity activity : section.getActivities()) {
+                        // Set Activity Type Code Description
+                        col = 5;
+                        excelHeader.createCell(col).setCellValue(activity.getActivityTypeCodeDescription());
+
+                        // Set Days
+                        col = 6;
+                        excelHeader.createCell(col).setCellValue(activity.getDayIndicator());
+
+                        // Set Start
+                        if (activity.getStartTime() != null) {
+                            col = 7;
+                            excelHeader.createCell(col).setCellValue(activity.getStartTime().toString());
+                        }
+
+                        // Set Start
+                        if (activity.getEndTime() != null) {
+                            col = 8;
+                            excelHeader.createCell(col).setCellValue(activity.getEndTime().toString());
+                        }
+
+                        if (activity.getLocationDescription() != null) {
+                            col = 9;
+                            excelHeader.createCell(col).setCellValue(activity.getLocationDescription());
+                        }
+
+                        row++;
+                        excelHeader = sheet.createRow(row);
+                    }
+
+                    if (section.getActivities().size() == 0) {
+                        row++;
+                        excelHeader = sheet.createRow(row);
+                    }
+                }
+            }
+
+            if (course.getSectionGroups().size() == 0) {
+                row++;
+            }
         }
     }
 
@@ -58,12 +116,14 @@ public class ScheduleSummaryReportExcelView extends AbstractXlsView {
         Row excelHeader = excelSheet.createRow(0);
 
         excelHeader.createCell(0).setCellValue("Course");
-        excelHeader.createCell(1).setCellValue("Tracks");
-
- //       int col = 2;
-//        for(Term term : courseViewDTO.getTerms()) {
- //           excelHeader.createCell(col).setCellValue(Term.getRegistrarName(term.getTermCode()));
- //           col++;
- //       }
+        excelHeader.createCell(1).setCellValue("Assigned");
+        excelHeader.createCell(2).setCellValue("Section");
+        excelHeader.createCell(3).setCellValue("CRN");
+        excelHeader.createCell(4).setCellValue("Seats");
+        excelHeader.createCell(5).setCellValue("Activity");
+        excelHeader.createCell(6).setCellValue("Activity");
+        excelHeader.createCell(7).setCellValue("Start");
+        excelHeader.createCell(8).setCellValue("End");
+        excelHeader.createCell(9).setCellValue("Location");
     }
 }
