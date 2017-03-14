c5ab81257079b2d663c981ab118e60102dab2c08
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-03-14 16:08:46 -0700

    InstructionalSupport: Collapsing supportCalls into responses

--- src/main/resources/db/migration/V141__Add_InstructionalSupportStaff.sql
+++ src/main/resources/db/migration/V141__Add_InstructionalSupportStaff.sql
@@ -23,7 +23,6 @@ CREATE TABLE `StudentSupportPreferences` (
   `Id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `SupportStaffId` int(11) NOT NULL,
   `SectionGroupId` int(11) NOT NULL,
-  `StudentSupportCallId` int(11) NOT NULL,
   `Comment` varchar(20) DEFAULT NULL,
   `Type` varchar(20) NOT NULL,
   `Priority` int(11) NOT NULL,
@@ -38,7 +37,6 @@ CREATE TABLE `InstructorSupportPreferences` (
   `InstructorId` int(11) NOT NULL,
   `SupportStaffId` int(11) NOT NULL,
   `SectionGroupId` int(11) NOT NULL,
-  `InstructorSupportCallId` int(11) NOT NULL,
   `Priority` int(11) NOT NULL,
   KEY `InstructorSupportPreferences_SectionGroupId_idx` (`SectionGroupId`),
   KEY `InstructorSupportPreferences_SupportStaffId_idx` (`SupportStaffId`),
@@ -46,13 +44,18 @@ CREATE TABLE `InstructorSupportPreferences` (
   CONSTRAINT `InstructorSupportPreferences_SectionGroupId` FOREIGN KEY (`SectionGroupId`) REFERENCES `SectionGroups` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 
-CREATE TABLE `StudentSupportCalls` (
+CREATE TABLE `StudentSupportCallResponses` (
   `Id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `LastContactedAt` timestamp NULL DEFAULT NULL,
+  `NextContactAt` timestamp NULL DEFAULT NULL,
+  `Submitted` tinyint(1) NOT NULL DEFAULT '0',
+  `SupportStaffId` int(11) NOT NULL,
+  `GeneralComments` text,
+  `TeachingQualifications` text,
   `ScheduleId` int(11) NOT NULL,
   `StartDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `DueDate` timestamp NOT NULL DEFAULT '1970-01-01 00:00:01',
   `Message` text,
-  `SendEmails` tinyint(1) NOT NULL DEFAULT '0',
   `AllowSubmissionAfterDueDate` tinyint(1) NOT NULL DEFAULT '1',
   `TermCode` varchar(6) NOT NULL,
   `MinimumNumberOfPreferences` int(11) NOT NULL DEFAULT 0,
@@ -63,46 +66,24 @@ CREATE TABLE `StudentSupportCalls` (
   `CollectTeachingAssistantPreferences` tinyint(1) NOT NULL DEFAULT '1',
   `CollectReaderPreferences` tinyint(1) NOT NULL DEFAULT '1',
   `CollectAssociateInstructorPreferences` tinyint(1) NOT NULL DEFAULT '1',
-  KEY `StudentSupportCalls_ScheduleId_idx` (`ScheduleId`),
-  CONSTRAINT `StudentSupportCalls_ScheduleId` FOREIGN KEY (`ScheduleId`) REFERENCES `Schedules` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-);
 
-
-CREATE TABLE `InstructorSupportCalls` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `ScheduleId` int(11) NOT NULL,
-  `StartDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `DueDate` timestamp NOT NULL DEFAULT '1970-01-01 00:00:01',
-  `Message` text,
-  `SendEmails` tinyint(1) NOT NULL DEFAULT '0',
-  `AllowSubmissionAfterDueDate` tinyint(1) NOT NULL DEFAULT '1',
-  `TermCode` varchar(6) NOT NULL,
-  KEY `InstructorSupportCalls_ScheduleId_idx` (`ScheduleId`),
-  CONSTRAINT `InstructorSupportCalls_ScheduleId` FOREIGN KEY (`ScheduleId`) REFERENCES `Schedules` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-);
-
-
-CREATE TABLE `StudentSupportCallResponses` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `NotifiedAt` timestamp NULL DEFAULT NULL,
-  `WarnedAt` timestamp NULL DEFAULT NULL,
-  `Submitted` tinyint(1) NOT NULL DEFAULT '0',
-  `StudentSupportCallId` int(11) NOT NULL,
-  `SupportStaffId` int(11) NOT NULL,
-  `GeneralComments` text,
-  `TeachingQualifications` text,
   KEY `StudentSupportResponses_SupportStaffId_idx` (`SupportStaffId`),
   CONSTRAINT `StudentSupportResponses_InstructionalSupportStaffId` FOREIGN KEY (`SupportStaffId`) REFERENCES `SupportStaff` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 
 CREATE TABLE `InstructorSupportCallResponses` (
   `Id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `NotifiedAt` timestamp NULL DEFAULT NULL,
-  `WarnedAt` timestamp NULL DEFAULT NULL,
+  `LastContactedAt` timestamp NULL DEFAULT NULL,
+  `NextContactAt` timestamp NULL DEFAULT NULL,
   `Submitted` tinyint(1) NOT NULL DEFAULT '0',
-  `InstructorInstructionalSupportCallId` int(11) NOT NULL,
   `InstructorId` int(11) NOT NULL,
   `GeneralComments` text,
+  `StartDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `DueDate` timestamp NOT NULL DEFAULT '1970-01-01 00:00:01',
+  `Message` text,
+  `AllowSubmissionAfterDueDate` tinyint(1) NOT NULL DEFAULT '1',
+  `ScheduleId` int(11) NOT NULL,
+  `TermCode` varchar(6) NOT NULL,
   KEY `InstructorSupportResponses_InstructorId_idx` (`InstructorId`),
   CONSTRAINT `InstructorSupportResponses_InstructorId` FOREIGN KEY (`InstructorId`) REFERENCES `Instructors` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
