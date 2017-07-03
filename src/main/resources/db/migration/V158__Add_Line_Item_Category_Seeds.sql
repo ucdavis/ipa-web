b9c2d2d76244328079d90ad6306c3171270dfa7c
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-07-03 08:39:35 -0700

    BudgetView: Seed line item categories

---
+++ src/main/resources/db/migration/V158__Add_Line_Item_Category_Seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO `LineItemCategories` (`Description`) VALUES ('Additional Funds From Deans Office');
+INSERT INTO `LineItemCategories` (`Description`) VALUES ('Buyout Lecturer Replacement Funds');
+INSERT INTO `LineItemCategories` (`Description`) VALUES ('Class Cancelled - Funds no longer needed');
+INSERT INTO `LineItemCategories` (`Description`) VALUES ('TA Range Adjustment Funds');
+INSERT INTO `LineItemCategories` (`Description`) VALUES ('Work-Life Balance Funds');
+INSERT INTO `LineItemCategories` (`Description`) VALUES ('Other Funds');
