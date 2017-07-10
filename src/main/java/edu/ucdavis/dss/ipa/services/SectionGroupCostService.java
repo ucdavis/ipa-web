326eef42ccc0e81049dbe7bcc365ecca81647cd2
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-07-10 03:52:33 -0700

    BudgetView: Create service to clone sectionGroupCosts

--- src/main/java/edu/ucdavis/dss/ipa/services/SectionGroupCostService.java
+++ src/main/java/edu/ucdavis/dss/ipa/services/SectionGroupCostService.java
@@ -2,14 +2,12 @@ package edu.ucdavis.dss.ipa.services;
 
 import java.util.List;
 
-import edu.ucdavis.dss.ipa.entities.Course;
-import edu.ucdavis.dss.ipa.entities.SectionGroupCost;
+import edu.ucdavis.dss.ipa.entities.*;
 import org.springframework.validation.annotation.Validated;
-
-import edu.ucdavis.dss.ipa.entities.Section;
-import edu.ucdavis.dss.ipa.entities.SectionGroup;
 
 @Validated
 public interface SectionGroupCostService {
     List<SectionGroupCost> findByBudgetId(Long budgetId);
+
+    SectionGroupCost createFrom(SectionGroupCost originalSectionGroupCost, BudgetScenario budgetScenario);
 }
--- src/main/java/edu/ucdavis/dss/ipa/services/jpa/JpaSectionGroupCostService.java
+++ src/main/java/edu/ucdavis/dss/ipa/services/jpa/JpaSectionGroupCostService.java
@@ -1,5 +1,6 @@
 package edu.ucdavis.dss.ipa.services.jpa;
 
+import edu.ucdavis.dss.ipa.entities.BudgetScenario;
 import edu.ucdavis.dss.ipa.entities.SectionGroupCost;
 import edu.ucdavis.dss.ipa.repositories.SectionGroupCostRepository;
 import edu.ucdavis.dss.ipa.services.SectionGroupCostService;
@@ -15,5 +16,22 @@ public class JpaSectionGroupCostService implements SectionGroupCostService {
     @Override
     public List<SectionGroupCost> findByBudgetId(Long budgetId) {
         return sectionGroupCostRepository.findByBudgetId(budgetId);
+    }
+
+    @Override
+    public SectionGroupCost createFrom(SectionGroupCost originalSectionGroupCost, BudgetScenario budgetScenario) {
+        SectionGroupCost sectionGroupCost = new SectionGroupCost();
+
+        sectionGroupCost.setBudgetScenario(budgetScenario);
+        sectionGroupCost.setInstructor(originalSectionGroupCost.getInstructor());
+        sectionGroupCost.setSectionGroup(originalSectionGroupCost.getSectionGroup());
+        sectionGroupCost.setEnrollment(originalSectionGroupCost.getEnrollment());
+        sectionGroupCost.setOriginalInstructor(originalSectionGroupCost.getOriginalInstructor());
+        sectionGroupCost.setReaderCount(originalSectionGroupCost.getReaderCount());
+        sectionGroupCost.setTaCount(originalSectionGroupCost.getTaCount());
+        sectionGroupCost.setSectionCount(originalSectionGroupCost.getSectionCount());
+        sectionGroupCost.setInstructorCost(originalSectionGroupCost.getInstructorCost());
+
+        return this.sectionGroupCostRepository.save(sectionGroupCost);
     }
 }
