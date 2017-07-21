9f8eabbbc68fd1a990e5add9370175699de57425
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-07-21 10:06:08 -0700

    Add instructorCost entity

---
+++ src/main/java/edu/ucdavis/dss/ipa/entities/InstructorCost.java
@@ -0,0 +1,96 @@
+package edu.ucdavis.dss.ipa.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import edu.ucdavis.dss.ipa.api.deserializers.ActivityDeserializer;
+import edu.ucdavis.dss.ipa.api.deserializers.LineItemDeserializer;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "InstructorCosts")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@JsonDeserialize(using = LineItemDeserializer.class)
+public class InstructorCost {
+    private long id;
+    private Budget budget;
+    private Instructor instructor;
+    private float cost;
+    private Boolean lecturer;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id", unique = true, nullable = false)
+    @JsonProperty
+    public long getId()
+    {
+        return this.id;
+    }
+
+    public void setId(long id)
+    {
+        this.id = id;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "BudgetId", nullable = false)
+    @NotNull
+    @JsonIgnore
+    public Budget getBudget() {
+        return budget;
+    }
+
+    public void setBudget(Budget budget) {
+        this.budget = budget;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    public Boolean getLecturer() {
+        return lecturer;
+    }
+
+    public void setLecturer(Boolean lecturer) {
+        this.lecturer = lecturer;
+    }
+
+    @JsonProperty("instructorId")
+    @Transient
+    public long getInstructorId() {
+        if(instructor != null) {
+            return instructor.getId();
+        } else {
+            return 0;
+        }
+    }
+
+    @JsonProperty("budgetId")
+    @Transient
+    public long getBudgetId() {
+        if(budget != null) {
+            return budget.getId();
+        } else {
+            return 0;
+        }
+    }
+}
