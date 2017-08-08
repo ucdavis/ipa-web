d479f67001b206afa5c4c55bccbe42d6bd8c8e50
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-08-07 20:24:13 -0700

    Add helper to titleize names

--- src/main/java/edu/ucdavis/dss/ipa/api/helpers/Utilities.java
+++ src/main/java/edu/ucdavis/dss/ipa/api/helpers/Utilities.java
@@ -25,4 +25,30 @@ public class Utilities {
         }
         return null;
     }
+
+    /**
+     * Returns a new copy of the given string where each character immediately after
+     * a whitespace character is title-cased. Only works for characters in the
+     * @param input the string to be title-cased. May not be null.
+     * @return a new copy of the given string where each character immediately after
+     * a whitespace character is title-cased. Will return null if input is null.
+     */
+    public static String titleize(final String input) {
+        if (input == null) {
+            return null;
+        }
+
+        final StringBuilder output = new StringBuilder(input.length());
+        boolean lastCharacterWasWhitespace = true;
+
+        for (final char currentCharacter : input.toCharArray()) {
+            if (lastCharacterWasWhitespace) {
+                output.append(Character.toTitleCase(currentCharacter));
+            } else {
+                output.append(Character.toLowerCase(currentCharacter));
+            }
+            lastCharacterWasWhitespace = Character.isWhitespace(currentCharacter);
+        }
+        return output.toString();
+    }
 }
