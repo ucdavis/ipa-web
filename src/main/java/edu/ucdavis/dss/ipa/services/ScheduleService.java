56a757bc714b4eaebbfa640bd8d36ab40a829023
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-09-22 15:03:17 -0700

    DwCourseImport: Initial refactoring of courseImport logic

--- src/main/java/edu/ucdavis/dss/ipa/services/ScheduleService.java
+++ src/main/java/edu/ucdavis/dss/ipa/services/ScheduleService.java
@@ -2,6 +2,7 @@ package edu.ucdavis.dss.ipa.services;
 
 import java.util.List;
 
+import edu.ucdavis.dss.ipa.api.components.course.views.SectionGroupImport;
 import edu.ucdavis.dss.ipa.entities.*;
 import org.springframework.validation.annotation.Validated;
 
@@ -38,4 +39,6 @@ public interface ScheduleService {
 	 * @return
 	 */
 	List<Term> getActiveTermCodesForSchedule(Schedule schedule);
+
+	void importCoursesFromDW(long scheduleId, List<SectionGroupImport> sectionGroupImportList, Long destinationYear, Boolean importAssignments, Boolean importTimes, Boolean showDoNotPrint);
 }
