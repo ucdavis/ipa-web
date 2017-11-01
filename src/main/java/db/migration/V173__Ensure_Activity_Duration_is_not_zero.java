619cf7bcd93062d19ca835dd1b6110b15d468aff
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-11-01 10:08:35 -0700

    Refactoring migration to use SQL syntax

--- src/main/java/db/migration/V173__Ensure_Activity_Duration_is_not_zero.java
+++ src/main/java/db/migration/V173__Ensure_Activity_Duration_is_not_zero.java
@@ -11,50 +11,14 @@ public class V173__Ensure_Activity_Duration_is_not_zero implements JdbcMigration
 
     @Override
     public void migrate(Connection connection) throws Exception {
-        // Find all activities
-
-        PreparedStatement psActivities = connection.prepareStatement("SELECT * FROM `Activities`");
-
         try {
-            connection.setAutoCommit(false);
+            PreparedStatement psActivities = connection.prepareStatement("Update Activities SET EndTime = DATE_ADD(EndTime, INTERVAL 50 MINUTE) WHERE StartTime = EndTime");
 
-            ResultSet rsActivities = psActivities.executeQuery();
-
-            while (rsActivities.next()) {
-                long activityId = rsActivities.getLong("Id");
-                Time startTime = rsActivities.getTime("StartTime");
-                Time endTime = rsActivities.getTime("EndTime");
-
-                if (startTime == null || endTime == null) {
-                    continue;
-                }
-
-                if (startTime.equals(endTime) ) {
-                    // Add sectionGroup association
-                    PreparedStatement psActivity = connection.prepareStatement(
-                            " DELETE FROM `Activities` WHERE `Id` = ?;"
-                    );
-
-                    psActivity.setLong(1, activityId);
-                    psActivity.execute();
-                    psActivity.close();
-                }
-            }
-
-            // Commit changes
-            connection.commit();
-
+            psActivities.execute();
+            psActivities.close();
         } catch(SQLException e) {
             e.printStackTrace();
             return;
-        } finally {
-            try {
-                if (psActivities != null) {
-                    psActivities.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
