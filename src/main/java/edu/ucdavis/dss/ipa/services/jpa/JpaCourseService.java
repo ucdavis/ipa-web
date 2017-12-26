8df85a3df71fcec4927c0f449081555ad8b7b910
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-12-26 15:52:43 -0800

    Removing unused service injections

--- src/main/java/edu/ucdavis/dss/ipa/services/jpa/JpaCourseService.java
+++ src/main/java/edu/ucdavis/dss/ipa/services/jpa/JpaCourseService.java
@@ -19,13 +19,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class JpaCourseService implements CourseService {
 
 	@Inject CourseRepository courseRepository;
+
 	@Inject SectionGroupRepository sectionGroupRepository;
-	@Inject SectionGroupService sectionGroupService;
 	@Inject ScheduleService scheduleService;
-	@Inject ScheduleTermStateService scheduleTermStateService;
-	@Inject InstructorService instructorService;
 	@Inject TagService tagService;
-	@Inject WorkgroupService workgroupService;
 	@Inject SectionService sectionService;
 
 	@Override
