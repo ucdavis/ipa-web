0b15149ac817d46bb318f2c2a4583400e85d835a
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-01-29 16:52:37 -0800

    Drop unnecessary columns from sectionGroupCosts

---
+++ src/main/resources/db/migration/V181__Remove_unneeded_fields_from_sectionGroupCosts.sql
@@ -0,0 +1,9 @@
+ALTER TABLE `SectionGroupCosts` DROP COLUMN `Title`;
+ALTER TABLE `SectionGroupCosts` DROP COLUMN `SubjectCode`;
+ALTER TABLE `SectionGroupCosts` DROP COLUMN `CourseNumber`;
+ALTER TABLE `SectionGroupCosts` DROP COLUMN `SequencePattern`;
+ALTER TABLE `SectionGroupCosts` DROP COLUMN `TermCode`;
+ALTER TABLE `SectionGroupCosts` DROP COLUMN `UnitsLow`;
+ALTER TABLE `SectionGroupCosts` DROP COLUMN `UnitsHigh`;
+ALTER TABLE `SectionGroupCosts` DROP COLUMN `EffectiveTermCode`;
+ALTER TABLE `SectionGroupCosts` DROP COLUMN `Title`;
