f0fb2efdca6944acae3d9586f02f4b8d0e46da24
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-02-05 02:11:13 -0800

    Add instructorCost to payload

--- src/main/java/edu/ucdavis/dss/ipa/api/components/budget/views/factories/JpaBudgetViewFactory.java
+++ src/main/java/edu/ucdavis/dss/ipa/api/components/budget/views/factories/JpaBudgetViewFactory.java
@@ -2,12 +2,43 @@ package edu.ucdavis.dss.ipa.api.components.budget.views.factories;
 
 import edu.ucdavis.dss.ipa.api.components.budget.views.BudgetScenarioView;
 import edu.ucdavis.dss.ipa.api.components.budget.views.BudgetView;
-import edu.ucdavis.dss.ipa.entities.*;
-import edu.ucdavis.dss.ipa.services.*;
+import edu.ucdavis.dss.ipa.entities.Budget;
+import edu.ucdavis.dss.ipa.entities.BudgetScenario;
+import edu.ucdavis.dss.ipa.entities.Course;
+import edu.ucdavis.dss.ipa.entities.Instructor;
+import edu.ucdavis.dss.ipa.entities.InstructorCost;
+import edu.ucdavis.dss.ipa.entities.InstructorType;
+import edu.ucdavis.dss.ipa.entities.LineItem;
+import edu.ucdavis.dss.ipa.entities.LineItemCategory;
+import edu.ucdavis.dss.ipa.entities.LineItemComment;
+import edu.ucdavis.dss.ipa.entities.Schedule;
+import edu.ucdavis.dss.ipa.entities.Section;
+import edu.ucdavis.dss.ipa.entities.SectionGroup;
+import edu.ucdavis.dss.ipa.entities.SectionGroupCost;
+import edu.ucdavis.dss.ipa.entities.SectionGroupCostComment;
+import edu.ucdavis.dss.ipa.entities.SupportAssignment;
+import edu.ucdavis.dss.ipa.entities.TeachingAssignment;
+import edu.ucdavis.dss.ipa.entities.User;
+import edu.ucdavis.dss.ipa.entities.Workgroup;
+import edu.ucdavis.dss.ipa.services.CourseService;
+import edu.ucdavis.dss.ipa.services.InstructorCostService;
+import edu.ucdavis.dss.ipa.services.InstructorService;
+import edu.ucdavis.dss.ipa.services.InstructorTypeService;
+import edu.ucdavis.dss.ipa.services.LineItemCategoryService;
+import edu.ucdavis.dss.ipa.services.LineItemCommentService;
+import edu.ucdavis.dss.ipa.services.LineItemService;
+import edu.ucdavis.dss.ipa.services.ScheduleService;
+import edu.ucdavis.dss.ipa.services.SectionGroupCostCommentService;
+import edu.ucdavis.dss.ipa.services.SectionGroupCostService;
+import edu.ucdavis.dss.ipa.services.SectionGroupService;
+import edu.ucdavis.dss.ipa.services.SectionService;
+import edu.ucdavis.dss.ipa.services.SupportAssignmentService;
+import edu.ucdavis.dss.ipa.services.TeachingAssignmentService;
+import edu.ucdavis.dss.ipa.services.UserService;
+import edu.ucdavis.dss.ipa.services.WorkgroupService;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -29,6 +60,7 @@ public class JpaBudgetViewFactory implements BudgetViewFactory {
     @Inject SupportAssignmentService supportAssignmentService;
     @Inject TeachingAssignmentService teachingAssignmentService;
     @Inject UserService userService;
+    @Inject InstructorTypeService instructorTypeService;
 
     @Override
     public BudgetView createBudgetView(long workgroupId, long year, Budget budget) {
@@ -43,6 +75,7 @@ public class JpaBudgetViewFactory implements BudgetViewFactory {
         List<Course> courses = courseService.findVisibleByWorkgroupIdAndYear(workgroupId, year);
         List<SectionGroup> sectionGroups = sectionGroupService.findByCourses(courses);
         List<InstructorCost> instructorCosts = instructorCostService.findOrCreateManyFromBudget(budget);
+        List<InstructorType> instructorTypes = instructorTypeService.findByBudgetId(budget.getId());
 
         List<Instructor> instructors = instructorService.findByInstructorCosts(instructorCosts);
         List<Instructor> assignedInstructors = instructorService.findAssignedByScheduleId(schedule.getId());
@@ -72,7 +105,8 @@ public class JpaBudgetViewFactory implements BudgetViewFactory {
                 courses,
                 teachingAssignments,
                 supportAssignments,
-                users);
+                users,
+                instructorTypes);
 
         return budgetView;
     }
