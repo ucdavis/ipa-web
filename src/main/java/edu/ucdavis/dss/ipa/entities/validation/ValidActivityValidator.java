95cb5a8db30755a3abcc9775fbbd95b4485bef68
Christopher Thielen <cmthielen@ucdavis.edu>
2018-02-22 14:27:53 -0800

    Attempt to add error messages to activity validator

--- src/main/java/edu/ucdavis/dss/ipa/entities/validation/ValidActivityValidator.java
+++ src/main/java/edu/ucdavis/dss/ipa/entities/validation/ValidActivityValidator.java
@@ -16,11 +16,23 @@ public class ValidActivityValidator implements ConstraintValidator<ValidActivity
 		boolean sectionIsNull = (activity.getSection() == null);
 		boolean sectionGroupIsNull = (activity.getSectionGroup() == null);
 
+		// An activity can have a section or a sectionGroup, but not both
 		if (sectionGroupIsNull == sectionIsNull) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(
+					"only section or sectionGroup may be set"
+			)
+					.addConstraintViolation();
 			return false;
 		}
 
+		// If start and end times are both set, they cannot be the same value
 		if (activity.getStartTime() != null && activity.getEndTime() != null && activity.getStartTime().equals(activity.getEndTime())) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(
+					"start time and end time cannot be equal if set"
+			)
+					.addConstraintViolation();
 			return false;
 		}
 
