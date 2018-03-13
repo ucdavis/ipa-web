d1daeb4a222519068dfaa6b056e8ab9187eb90cb
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-03-13 15:48:31 -0700

    Fill in instructorType on historical teachingAssignments

---
+++ src/main/java/db/migration/V198__Migrate_TeachingAssignments_To_InstructorType.java
@@ -0,0 +1,94 @@
+package db.migration;
+
+import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class V198__Migrate_TeachingAssignments_To_InstructorType implements JdbcMigration {
+
+    /**
+     * Calculate instructorTypes for pre-existing teachingAssignments.
+     * @param connection
+     * @throws Exception
+     */
+    @Override
+    public void migrate(Connection connection) throws Exception {
+        PreparedStatement psTeachingAssignments = connection.prepareStatement("SELECT * FROM `TeachingAssignments`");
+
+        try {
+            connection.setAutoCommit(false);
+            ResultSet rsTeachingAssignments = psTeachingAssignments.executeQuery();
+
+            while(rsTeachingAssignments.next()) {
+                Long teachingAssignmentId = rsTeachingAssignments.getLong("Id");
+                Long instructorId = rsTeachingAssignments.getLong("InstructorId");
+                Long sectionGroupId = rsTeachingAssignments.getLong("SectionGroupId");
+
+                // Get workgroup
+                Long workgroupId = null;
+
+                PreparedStatement psWorkgroup = connection.prepareStatement(
+                    "SELECT Workgroups.Id FROM Workgroups, Schedules, Courses, SectionGroups WHERE SectionGroups.Id = ? AND SectionGroups.CourseId = Courses.Id AND Courses.ScheduleId = Schedules.Id AND Schedules.WorkgroupId = Workgroups.Id;"
+                );
+                psWorkgroup.setLong(1, sectionGroupId);
+                ResultSet rsWorkgroup = psWorkgroup.executeQuery();
+
+                while(rsWorkgroup.next()) {
+                    workgroupId = rsWorkgroup.getLong("Id");
+                }
+
+                // Get instructorType
+                Long instructorTypeId = null;
+
+                // If user based, look at userRoles to calculate
+                PreparedStatement psRoles = connection.prepareStatement("SELECT UserRoles.InstructorTypeId FROM Instructors, Users, UserRoles WHERE Instructors.Id = ? AND Instructors.LoginId = Users.LoginId AND UserRoles.UserId = Users.Id AND UserRoles.WorkgroupId = ?");
+                psRoles.setLong(1, instructorId);
+                psRoles.setLong(2, workgroupId);
+
+                ResultSet rsRoles = psRoles.executeQuery();
+
+                while(rsRoles.next()) {
+                    Long slotInstructorTypeId = rsRoles.getLong("InstructorTypeId");
+
+                    if (slotInstructorTypeId != null) {
+                        instructorTypeId = slotInstructorTypeId;
+                    }
+                }
+
+                // If relevant userRole wasn't found, set instructorType to the generic 'instructor' type.
+                if (instructorTypeId == null) {
+                    instructorTypeId = 7L;
+                }
+
+                // Update TeachingAssignment with instructorType
+                PreparedStatement psUpdateTeachingAssignment = connection.prepareStatement("UPDATE TeachingAssignments Set InstructorTypeId = ? WHERE TeachingAssignments.Id = ?");
+
+                psUpdateTeachingAssignment.setLong(1, instructorTypeId);
+                psUpdateTeachingAssignment.setLong(2, teachingAssignmentId);
+
+                psUpdateTeachingAssignment.execute();
+                psUpdateTeachingAssignment.close();
+            }
+
+            // Commit changes
+            connection.commit();
+
+        } catch(SQLException e) {
+            e.printStackTrace();
+            return;
+        } finally {
+            try {
+                if (psTeachingAssignments != null) {
+                    psTeachingAssignments.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
