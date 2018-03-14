53fdfa6825b52e38eb0e8b3f7ac941a3598a1775
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-03-14 11:03:38 -0700

    Fix database rows with null schedule review flags

---
+++ src/main/resources/db/migration/V189__Migrate_empty_schedule_review.sql
@@ -0,0 +1,2 @@
+UPDATE `Schedules` SET `SupportStaffSupportCallReviewOpen` = "0000000000" WHERE `SupportStaffSupportCallReviewOpen` IS NULL;
+UPDATE `Schedules` SET `InstructorSupportCallReviewOpen` = "0000000000" WHERE `InstructorSupportCallReviewOpen` IS NULL;
