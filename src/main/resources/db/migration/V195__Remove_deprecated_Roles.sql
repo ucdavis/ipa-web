109bce1db247dea1d5a843ff25becb1eb4130bfe
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-04-02 16:23:09 -0700

    Clarifying comment on migration

--- src/main/resources/db/migration/V195__Remove_deprecated_Roles.sql
+++ src/main/resources/db/migration/V195__Remove_deprecated_Roles.sql
@@ -1,10 +1,10 @@
-/* Remove userRoles based on the federationInstructor role */
+/* Remove userRoles based on the federationInstructor role of 8 */
 DELETE FROM `UserRoles` WHERE RoleId = 8;
 
-/* Remove userRoles based on the senateInstructor role */
+/* Remove userRoles based on the senateInstructor role of 1 */
 DELETE FROM `UserRoles` WHERE RoleId = 1;
 
-/* Remove userRoles based on the lecturer role */
+/* Remove userRoles based on the lecturer role of 14 */
 DELETE FROM `UserRoles` WHERE RoleId = 14;
 
 DELETE FROM `Roles` WHERE Name = 'senateInstructor';
