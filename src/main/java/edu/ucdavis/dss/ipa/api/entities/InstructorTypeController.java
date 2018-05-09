0af7eb4cbaccbe1e3e75cc599e6190001524c207
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-05-09 13:51:06 -0700

    Adding entity based crud api endpoints

---
+++ src/main/java/edu/ucdavis/dss/ipa/api/entities/InstructorTypeController.java
@@ -0,0 +1,28 @@
+package edu.ucdavis.dss.ipa.api.entities;
+
+import edu.ucdavis.dss.ipa.entities.InstructorType;
+import edu.ucdavis.dss.ipa.security.Authorizer;
+import edu.ucdavis.dss.ipa.services.InstructorTypeService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class InstructorTypeController {
+  @Inject InstructorTypeService instructorTypeService;
+  @Inject Authorizer authorizer;
+
+  @RequestMapping(value = "/api/instructorTypes", method = RequestMethod.GET, produces="application/json")
+  @ResponseBody
+  public List<InstructorType> getInstructorTypes() {
+    authorizer.isAuthorized();
+
+    return instructorTypeService.getAllInstructorTypes();
+  }
+}
---
+++ src/main/java/edu/ucdavis/dss/ipa/api/entities/TeachingAssignmentController.java
@@ -0,0 +1,40 @@
+package edu.ucdavis.dss.ipa.api.entities;
+
+import edu.ucdavis.dss.ipa.entities.Schedule;
+import edu.ucdavis.dss.ipa.entities.TeachingAssignment;
+import edu.ucdavis.dss.ipa.security.Authorizer;
+import edu.ucdavis.dss.ipa.services.ScheduleService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class TeachingAssignmentController {
+  @Inject ScheduleService scheduleService;
+  @Inject Authorizer authorizer;
+
+  @RequestMapping(value = "/api/schedules/{scheduleId}/teachingAssignments", method = RequestMethod.GET, produces="application/json")
+  @ResponseBody
+  public List<TeachingAssignment> getTeachingAssignments(@PathVariable long scheduleId,
+                                                         HttpServletResponse httpResponse) {
+    Schedule schedule = scheduleService.findById(scheduleId);
+
+    if (schedule == null) {
+      httpResponse.setStatus(HttpStatus.NOT_FOUND.value());
+      return null;
+
+    }
+    authorizer.hasWorkgroupRoles(schedule.getWorkgroup().getId(), "academicPlanner", "reviewer", "instructor", "studentPhd", "studentMasters", "instructionalSupport");
+
+    return schedule.getTeachingAssignments();
+  }
+}
