297f14e5a1699c4f9536c688b863e2d0bb0f7c06
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-08-07 10:27:52 -0700

    Fixing whitespace

--- src/main/java/edu/ucdavis/dss/ipa/entities/InstructorNote.java
+++ src/main/java/edu/ucdavis/dss/ipa/entities/InstructorNote.java
@@ -24,75 +24,75 @@ import edu.ucdavis.dss.ipa.api.deserializers.InstructorNoteDeserializer;
 @JsonDeserialize(using = InstructorNoteDeserializer.class)
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class InstructorNote extends BaseEntity {
-  private long id;
-  private String note;
-  private Instructor instructor;
-  private Schedule schedule;
+	private long id;
+	private String note;
+	private Instructor instructor;
+	private Schedule schedule;
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "Id", unique = true, nullable = false)
-  @JsonProperty
-  public long getId()
-  {
-    return this.id;
-  }
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "Id", unique = true, nullable = false)
+	@JsonProperty
+	public long getId()
+	{
+		return this.id;
+	}
 
-  public void setId(long id)
-  {
-    this.id = id;
-  }
+	public void setId(long id)
+	{
+		this.id = id;
+	}
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "ScheduleId", nullable = false)
-  @NotNull
-  @JsonIgnore
-  public Schedule getSchedule() {
-    return schedule;
-  }
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "ScheduleId", nullable = false)
+	@NotNull
+	@JsonIgnore
+	public Schedule getSchedule() {
+		return schedule;
+	}
 
-  public void setSchedule(Schedule schedule) {
-    this.schedule = schedule;
-  }
+	public void setSchedule(Schedule schedule) {
+		this.schedule = schedule;
+	}
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "InstructorId", nullable = false)
-  @NotNull
-  @JsonIgnore
-  public Instructor getInstructor() {
-    return instructor;
-  }
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "InstructorId", nullable = false)
+	@NotNull
+	@JsonIgnore
+	public Instructor getInstructor() {
+		return instructor;
+	}
 
-  public void setInstructor(Instructor instructor) {
-    this.instructor = instructor;
-  }
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
+	}
 
-  @JsonProperty
-  public String getNote() {
-    return note;
-  }
+	@JsonProperty
+	public String getNote() {
+		return note;
+	}
 
-  public void setNote(String note) {
-    this.note = note;
-  }
+	public void setNote(String note) {
+		this.note = note;
+	}
 
-  @JsonProperty("instructorId")
-  @Transient
-  public long getInstructorIdentification() {
-    if(instructor != null) {
-      return instructor.getId();
-    } else {
-      return 0;
-    }
-  }
+	@JsonProperty("instructorId")
+	@Transient
+	public long getInstructorIdentification() {
+		if(instructor != null) {
+			return instructor.getId();
+		} else {
+			return 0;
+		}
+	}
 
-  @JsonProperty("scheduleId")
-  @Transient
-  public long getScheduleIdentification() {
-    if(schedule != null) {
-      return schedule.getId();
-    } else {
-      return 0;
-    }
-  }
+	@JsonProperty("scheduleId")
+	@Transient
+	public long getScheduleIdentification() {
+		if(schedule != null) {
+			return schedule.getId();
+		} else {
+			return 0;
+		}
+	}
 }
