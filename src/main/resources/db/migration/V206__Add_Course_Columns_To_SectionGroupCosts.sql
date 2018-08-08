77f2c5b71b4d2a516ee10db7e788dc273822a584
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-08-08 12:30:33 -0700

    Add course data columns to sectionGroupCosts

---
+++ src/main/resources/db/migration/V206__Add_Course_Columns_To_SectionGroupCosts.sql
@@ -0,0 +1,9 @@
+ALTER TABLE `SectionGroupCosts` ADD COLUMN `Title` varchar(100) NOT NULL;
+ALTER TABLE `SectionGroupCosts` ADD COLUMN `SubjectCode` varchar(4) NOT NULL;
+ALTER TABLE `SectionGroupCosts` ADD COLUMN `CourseNumber` varchar(7) NOT NULL;
+ALTER TABLE `SectionGroupCosts` ADD COLUMN `EffectiveTermCode` varchar(6) NOT NULL;
+ALTER TABLE `SectionGroupCosts` ADD COLUMN `SequencePattern` varchar(3) NOT NULL;
+ALTER TABLE `SectionGroupCosts` ADD COLUMN `TermCode` varchar(6) NOT NULL;
+ALTER TABLE `SectionGroupCosts` ADD COLUMN `UnitsLow` FLOAT NOT NULL;
+ALTER TABLE `SectionGroupCosts` ADD COLUMN `UnitsHigh` FLOAT NOT NULL;
+ALTER TABLE `SectionGroupCosts` ADD COLUMN `EffectiveTermCode` varchar(6) NOT NULL;
