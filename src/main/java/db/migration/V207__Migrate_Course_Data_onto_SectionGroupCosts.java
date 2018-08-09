daa24f9d86192651ca602238c791283079f38ea9
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-08-09 16:46:27 -0700

    Add migration to fill in course data on sectionGroupCosts

--- src/main/java/db/migration/V207__Migrate_Course_Data_onto_SectionGroupCosts.java
+++ src/main/java/db/migration/V207__Migrate_Course_Data_onto_SectionGroupCosts.java
@@ -17,12 +17,7 @@ public class V207__Migrate_Course_Data_onto_SectionGroupCosts implements JdbcMig
 	@Override
 	public void migrate(Connection connection) throws Exception {
 		System.out.println("[MIGRATION] 207 START");
-		// For each sectionGroup
-		// For each budgetScenario in the same schedule as the sectionGroup
-		// Query for a sectionGroupCost that matches the sectionGroupId and budgetScenarioId
-		// If found, update all null fields on the sectionGroupCost with current data (from sectionGroup and course)
-		// If note found, create and mirror all fields (from sectionGroup and course)
-
+		// For every sectionGroupCost, find the relevant sectionGroup, and course, and copy data off them
 
 		PreparedStatement psSectionGroupCosts = connection.prepareStatement("SELECT * FROM SectionGroupCosts;");
 		connection.setAutoCommit(false);
@@ -34,13 +29,76 @@ public class V207__Migrate_Course_Data_onto_SectionGroupCosts implements JdbcMig
 		rsSectionGroupCosts.first();
 
 		while(rsSectionGroupCosts.next()) {
+			Long sectionGroupCostId = rsSectionGroupCosts.getLong("Id");
+			Long sectionGroupId = rsSectionGroupCosts.getLong("SectionGroupId");
+
 			currentSectionGroupCost += 1;
 
 			// TODO: logic goes here
+			String title = null;
+			String subjectCode = null;
+			String courseNumber = null;
+			String effectiveTermCode = null;
+			String sequencePattern = null;
+			String termCode = null;
+			Float unitsLow = null;
+			Float unitsHigh = null;
+			Boolean disabled = false;
 
-			if (currentSectionGroupCost % 1000 == 0) {
-				System.out.println("sectionGroups processed: " + currentSectionGroupCost + " / " + totalSectionGroupCosts);
-				System.out.println( Math.round(((currentSectionGroupCost * 1.0 / totalSectionGroupCosts) * 100)) + "%");
+			PreparedStatement psSectionGroup = connection.prepareStatement("SELECT * FROM SectionGroups sg WHERE sg.Id = ?;");
+			psSectionGroup.setLong(1, sectionGroupId);
+			ResultSet rsSectionGroup = psSectionGroup.executeQuery();
+
+			Long courseId = null;
+			while(rsSectionGroup.next()) {
+				termCode = rsSectionGroupCosts.getString("TermCode");
+				courseId = rsSectionGroupCosts.getLong("CourseId");
+
+				PreparedStatement psCourse = connection.prepareStatement("SELECT * FROM Courses c WHERE c.Id = ?;");
+				psCourse.setLong(1, courseId);
+				ResultSet rsCourse = psCourse.executeQuery();
+
+				while(rsCourse.next()) {
+					title = rsSectionGroupCosts.getString("Title");
+					courseNumber = rsSectionGroupCosts.getString("CourseNumber");
+					subjectCode = rsSectionGroupCosts.getString("SubjectCode");
+					sequencePattern = rsSectionGroupCosts.getString("SequencePattern");
+					effectiveTermCode = rsSectionGroupCosts.getString("EffectiveTermCode");
+					unitsLow = rsSectionGroupCosts.getFloat("UnitsLow");
+					unitsHigh = rsSectionGroupCosts.getFloat("UnitsHigh");
+				}
+
+				PreparedStatement psUpdateSectionGroupCost = connection.prepareStatement(
+					" UPDATE `SectionGroupCosts`" +
+						" SET `Title` = ?," +
+						" SET `CourseNumber` = ?," +
+						" SET `SubjectCode` = ?," +
+						" SET `SequenceNumber` = ?," +
+						" SET `EffectiveTermCode` = ?," +
+						" SET `UnitsLow` = ?," +
+						" SET `UnitsHigh` = ?," +
+						" SET `TermCode` = ?," +
+						" WHERE `Id` = ?;"
+				);
+
+				psUpdateSectionGroupCost.setString(1, title);
+				psUpdateSectionGroupCost.setString(2, courseNumber);
+				psUpdateSectionGroupCost.setString(3, subjectCode);
+				psUpdateSectionGroupCost.setString(4, sequencePattern);
+				psUpdateSectionGroupCost.setString(5, effectiveTermCode);
+				psUpdateSectionGroupCost.setFloat(6, unitsLow);
+				psUpdateSectionGroupCost.setFloat(7, unitsHigh);
+				psUpdateSectionGroupCost.setString(8, termCode);
+				psUpdateSectionGroupCost.setLong(9, sectionGroupCostId);
+
+				psUpdateSectionGroupCost.execute();
+				psUpdateSectionGroupCost.close();
+
+
+				if (currentSectionGroupCost % 1000 == 0) {
+					System.out.println("sectionGroups processed: " + currentSectionGroupCost + " / " + totalSectionGroupCosts);
+					System.out.println(Math.round(((currentSectionGroupCost * 1.0 / totalSectionGroupCosts) * 100)) + "%");
+				}
 			}
 		}
 
