9f283db7b36b7b40bf2a1dff53dbf9672d00296d
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-08-10 13:30:22 -0700

    Fixing course data migration bugs

--- src/main/java/db/migration/V207__Migrate_Course_Data_onto_SectionGroupCosts.java
+++ src/main/java/db/migration/V207__Migrate_Course_Data_onto_SectionGroupCosts.java
@@ -20,16 +20,10 @@ public class V207__Migrate_Course_Data_onto_SectionGroupCosts implements JdbcMig
 		connection.setAutoCommit(false);
 
 		ResultSet rsSectionGroupCosts = psSectionGroupCosts.executeQuery();
-		int currentSectionGroupCost = 0;
-		rsSectionGroupCosts.last();
-		int totalSectionGroupCosts = rsSectionGroupCosts.getRow();
-		rsSectionGroupCosts.first();
 
 		while(rsSectionGroupCosts.next()) {
 			Long sectionGroupCostId = rsSectionGroupCosts.getLong("Id");
 			Long sectionGroupId = rsSectionGroupCosts.getLong("SectionGroupId");
-
-			currentSectionGroupCost += 1;
 
 			String title = null;
 			String subjectCode = null;
@@ -39,62 +33,73 @@ public class V207__Migrate_Course_Data_onto_SectionGroupCosts implements JdbcMig
 			String termCode = null;
 			Float unitsLow = null;
 			Float unitsHigh = null;
-			Boolean disabled = false;
 
 			PreparedStatement psSectionGroup = connection.prepareStatement("SELECT * FROM SectionGroups sg WHERE sg.Id = ?;");
 			psSectionGroup.setLong(1, sectionGroupId);
 			ResultSet rsSectionGroup = psSectionGroup.executeQuery();
 
 			Long courseId = null;
+
 			while(rsSectionGroup.next()) {
 				termCode = rsSectionGroup.getString("TermCode");
 				courseId = rsSectionGroup.getLong("CourseId");
-
-				PreparedStatement psCourse = connection.prepareStatement("SELECT * FROM Courses c WHERE c.Id = ?;");
-				psCourse.setLong(1, courseId);
-				ResultSet rsCourse = psCourse.executeQuery();
-
-				while(rsCourse.next()) {
-					title = rsSectionGroupCosts.getString("Title");
-					courseNumber = rsSectionGroupCosts.getString("CourseNumber");
-					subjectCode = rsSectionGroupCosts.getString("SubjectCode");
-					sequencePattern = rsSectionGroupCosts.getString("SequencePattern");
-					effectiveTermCode = rsSectionGroupCosts.getString("EffectiveTermCode");
-					unitsLow = rsSectionGroupCosts.getFloat("UnitsLow");
-					unitsHigh = rsSectionGroupCosts.getFloat("UnitsHigh");
-				}
-
-				PreparedStatement psUpdateSectionGroupCost = connection.prepareStatement(
-					" UPDATE `SectionGroupCosts`" +
-					" SET `Title` = ?," +
-					" SET `CourseNumber` = ?," +
-					" SET `SubjectCode` = ?," +
-					" SET `SequenceNumber` = ?," +
-					" SET `EffectiveTermCode` = ?," +
-					" SET `UnitsLow` = ?," +
-					" SET `UnitsHigh` = ?," +
-					" SET `TermCode` = ?," +
-					" WHERE `Id` = ?;"
-				);
-
-				psUpdateSectionGroupCost.setString(1, title);
-				psUpdateSectionGroupCost.setString(2, courseNumber);
-				psUpdateSectionGroupCost.setString(3, subjectCode);
-				psUpdateSectionGroupCost.setString(4, sequencePattern);
-				psUpdateSectionGroupCost.setString(5, effectiveTermCode);
-				psUpdateSectionGroupCost.setFloat(6, unitsLow);
-				psUpdateSectionGroupCost.setFloat(7, unitsHigh);
-				psUpdateSectionGroupCost.setString(8, termCode);
-				psUpdateSectionGroupCost.setLong(9, sectionGroupCostId);
-
-				psUpdateSectionGroupCost.execute();
-				psUpdateSectionGroupCost.close();
-
-				if (currentSectionGroupCost % 1000 == 0) {
-					System.out.println("sectionGroups processed: " + currentSectionGroupCost + " / " + totalSectionGroupCosts);
-					System.out.println(Math.round(((currentSectionGroupCost * 1.0 / totalSectionGroupCosts) * 100)) + "%");
-				}
 			}
+
+			// SectionGroupCost had been orphaned and should be removed
+			// No sectionGroup was associated with it, and hence no course data
+			if (courseId == null) {
+				PreparedStatement psDeleteSectionGroupCost = connection.prepareStatement("DELETE FROM `SectionGroupCosts` WHERE `Id` = ?;");
+				psDeleteSectionGroupCost.setLong(1, sectionGroupCostId);
+				psDeleteSectionGroupCost.execute();
+				psDeleteSectionGroupCost.close();
+				continue;
+			}
+
+			if (courseId == null) {
+				System.out.println("debug");
+			}
+
+			PreparedStatement psCourse = connection.prepareStatement("SELECT * FROM Courses c WHERE c.Id = ?;");
+			psCourse.setLong(1, courseId);
+			ResultSet rsCourse = psCourse.executeQuery();
+
+			while(rsCourse.next()) {
+				title = rsCourse.getString("Title");
+				courseNumber = rsCourse.getString("CourseNumber");
+				subjectCode = rsCourse.getString("SubjectCode");
+				sequencePattern = rsCourse.getString("SequencePattern");
+				effectiveTermCode = rsCourse.getString("EffectiveTermCode");
+				unitsLow = rsCourse.getFloat("UnitsLow");
+				unitsHigh = rsCourse.getFloat("UnitsHigh");
+			}
+
+			PreparedStatement psUpdateSectionGroupCost = connection.prepareStatement(
+				" UPDATE `SectionGroupCosts`" +
+				" SET `Title` = ?," +
+				" SET `CourseNumber` = ?," +
+				" SET `SubjectCode` = ?," +
+				" SET `SequencePattern` = ?," +
+				" SET `EffectiveTermCode` = ?," +
+				" SET `UnitsLow` = ?," +
+				" SET `UnitsHigh` = ?," +
+				" SET `TermCode` = ?," +
+				" WHERE `Id` = ?;"
+			);
+
+			System.out.println("taco");
+
+			psUpdateSectionGroupCost.setString(1, title);
+			psUpdateSectionGroupCost.setString(2, courseNumber);
+			psUpdateSectionGroupCost.setString(3, subjectCode);
+			psUpdateSectionGroupCost.setString(4, "000");
+			psUpdateSectionGroupCost.setString(5, effectiveTermCode);
+			psUpdateSectionGroupCost.setFloat(6, unitsLow);
+			psUpdateSectionGroupCost.setFloat(7, unitsHigh);
+			psUpdateSectionGroupCost.setString(8, termCode);
+			psUpdateSectionGroupCost.setLong(9, sectionGroupCostId);
+
+			psUpdateSectionGroupCost.execute();
+			psUpdateSectionGroupCost.close();
 		}
 
 		// Commit changes
