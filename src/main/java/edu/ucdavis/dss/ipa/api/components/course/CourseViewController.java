a606758bb989a08c4d23dbf34317cc88bc9a220a
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-09-17 09:33:21 -0700

    Fix bug in mass DW course import not properly saving shared activities

--- src/main/java/edu/ucdavis/dss/ipa/api/components/course/CourseViewController.java
+++ src/main/java/edu/ucdavis/dss/ipa/api/components/course/CourseViewController.java
@@ -415,6 +415,58 @@ public class CourseViewController {
 
 		List<DwSection> dwSections = dwRepository.getSectionsBySubjectCodeAndYear(subjectCode, yearToImportFrom);
 
+		// Group dwSections into related groups
+		// Mapped to a key (example : 'ECS030A')
+		Map<String, List<DwSection>> dwSectionGroups = new HashMap<String, List<DwSection>>();
+		List<String> dwSectionKeys = new ArrayList<>();
+
+		for (DwSection dwSection : dwSections) {
+			Character c = dwSection.getSequenceNumber().charAt(0);
+
+			// Ignore numeric sections
+			if (Character.isLetter(c) == false) { continue; }
+
+			String dwSectionKey = dwSection.getSubjectCode() + dwSection.getCourseNumber() + String.valueOf(c);
+			List<DwSection> sections = dwSectionGroups.get(dwSectionKey) != null ? dwSectionGroups.get(dwSectionKey) : new ArrayList<>();
+			sections.add(dwSection);
+
+			if (dwSectionKeys.contains(dwSectionKey) == false) {
+				dwSectionKeys.add(dwSectionKey);
+			}
+		}
+
+		// Identify activities that are 'shared'
+		List<String> sharedActivityKeys = new ArrayList<>();
+
+		for (String dwSectionKey: dwSectionKeys) {
+			Map<String, Long> activityKeyCounts = new HashMap<String, Long>();
+			List<String> activityKeys = new ArrayList<>();
+
+			for (DwSection dwSection : dwSectionGroups.get(dwSectionKey)) {
+				String sequencePattern = String.valueOf(dwSection.getSequenceNumber().charAt(0));
+
+				for (DwActivity dwActivity : dwSection.getActivities()) {
+					String startTime = dwActivity.getSsrmeet_begin_time() != null ? dwActivity.getSsrmeet_begin_time() : "";
+					String endTime = dwActivity.getSsrmeet_end_time() != null ? dwActivity.getSsrmeet_end_time() : "";
+					String activityType = String.valueOf(dwActivity.getSsrmeet_schd_code());
+					String dayIndicator = dwActivity.getDay_indicator() != null ? dwActivity.getDay_indicator() : "";
+					String activityKey = dwSection.getSubjectCode() + dwSection.getCourseNumber() + sequencePattern + activityType + dayIndicator + startTime + endTime;
+
+					activityKeys.add(activityKey);
+					Long activityCount = activityKeyCounts.get(activityKey) != null ? activityKeyCounts.get(activityKey) : 0L;
+					activityCount += 1;
+					activityKeyCounts.put(activityKey, activityCount);
+				}
+			}
+
+			for (String activityKey : activityKeys) {
+				if (activityKeyCounts.get(activityKey) == dwSectionGroups.get(dwSectionKey).size()) {
+					sharedActivityKeys.add(activityKey);
+				}
+			}
+
+		}
+
 		for (SectionGroupImport sectionGroupImport : sectionGroupImportList) {
 			for (DwSection dwSection : dwSections) {
 				// Calculate sequencePattern from sequenceNumber
@@ -523,10 +575,16 @@ public class CourseViewController {
 						activity.setActivityState(ActivityState.DRAFT);
 
 						// Activities in numeric sectionGroups should always be 'shared' activities
-						if (Utilities.isNumeric(dwSequencePattern)) {
-							activity.setSectionGroup(sectionGroup);
-						} else {
+						String startTime = dwActivity.getSsrmeet_begin_time() != null ? dwActivity.getSsrmeet_begin_time() : "";
+						String endTime = dwActivity.getSsrmeet_end_time() != null ? dwActivity.getSsrmeet_end_time() : "";
+						String dayIndicator = dwActivity.getDay_indicator() != null ? dwActivity.getDay_indicator() : "";
+						String sequencePattern = String.valueOf(dwSection.getSequenceNumber().charAt(0));
+						String activityKey = dwSection.getSubjectCode() + dwSection.getCourseNumber() + sequencePattern + activityType + dayIndicator + startTime + endTime;
+
+						if (sharedActivityKeys.indexOf(activityKey) == -1) {
 							activity.setSection(section);
+						} else {
+							activity.setSectionGroup(sectionGroup);
 						}
 
 						activityService.saveActivity(activity);
