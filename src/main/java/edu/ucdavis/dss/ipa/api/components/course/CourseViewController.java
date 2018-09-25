ebd33e6cd398470c83dcf540290e63829d4573b7
Sadaf Arshad <sarshad@ucdavis.edu>
2018-09-25 13:52:56 -0700

    Fix more than one return instructor element and NPE.

--- src/main/java/edu/ucdavis/dss/ipa/api/components/course/CourseViewController.java
+++ src/main/java/edu/ucdavis/dss/ipa/api/components/course/CourseViewController.java
@@ -604,11 +604,18 @@ public class CourseViewController {
 						}
 					}
 
+
 					if (importAssignments) {
 						for (DwInstructor dwInstructor : dwSection.getInstructors()) {
+
+							if (dwInstructor.getEmployeeId().equals("989999999")) {
+								sectionGroup.setShowTheStaff(true);
+								continue;
+							}
+
 							DwPerson dwPerson = dwRepository.getPersonByLoginId(dwInstructor.getLoginId());
 
-							if (dwPerson == null) {
+							if ((dwPerson == null) || (dwPerson.getUserId() == null && dwPerson.getoFullName() == null)) {
 								log.warn("getPersonByLoginId Response from DW returned null, for criterion = " + dwInstructor.getLoginId());
 								continue;
 							}
@@ -616,12 +623,12 @@ public class CourseViewController {
 							String instructorEmail = dwPerson.getEmail();
 
 							// Find or create an instructor
-							Instructor instructor = instructorService.findOrCreate(dwInstructor.getFirstName(), dwInstructor.getLastName(), instructorEmail, dwInstructor.getLoginId(), workgroupId);
+							Instructor instructor = instructorService.findOrCreate(dwInstructor.getFirstName(), dwInstructor.getLastName(), instructorEmail, dwInstructor.getLoginId(), workgroupId, dwInstructor.getEmployeeId());
 
 							// Find or create a teachingAssignment
 							TeachingAssignment teachingAssignment = teachingAssignmentService.findOrCreateOneBySectionGroupAndInstructor(sectionGroup, instructor);
 							teachingAssignment.setApproved(true);
-							teachingAssignment = teachingAssignmentService.saveAndAddInstructorType(teachingAssignment);
+							teachingAssignmentService.saveAndAddInstructorType(teachingAssignment);
 						}
 					}
 				}
