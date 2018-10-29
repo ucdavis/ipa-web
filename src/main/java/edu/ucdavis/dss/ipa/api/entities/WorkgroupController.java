0839949710641a3821a101613ab94f14a016f506
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-10-29 13:52:54 -0700

    Add remaining crud routes to workgroup entity controller

--- src/main/java/edu/ucdavis/dss/ipa/api/entities/WorkgroupController.java
+++ src/main/java/edu/ucdavis/dss/ipa/api/entities/WorkgroupController.java
@@ -5,6 +5,8 @@ import edu.ucdavis.dss.ipa.security.Authorizer;
 import edu.ucdavis.dss.ipa.services.UserRoleService;
 import edu.ucdavis.dss.ipa.services.WorkgroupService;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -27,5 +29,33 @@ public class WorkgroupController {
         authorizer.isAdmin();
 
         return workgroupService.findAll();
+    }
+
+    @RequestMapping(value = "/api/workgroups", method = RequestMethod.POST, produces="application/json")
+    @ResponseBody
+    public Workgroup addWorkgroup(@RequestBody Workgroup workgroup) {
+        authorizer.isAdmin();
+
+        return workgroupService.save(workgroup);
+    }
+
+    @RequestMapping(value = "/api/workgroups/{workgroupId}", method = RequestMethod.PUT, produces="application/json")
+    @ResponseBody
+    public Workgroup updateWorkgroup(@PathVariable Long workgroupId, @RequestBody Workgroup workgroup) {
+        authorizer.isAdmin();
+
+        Workgroup editedWorkgroup = workgroupService.findOneById(workgroupId);
+        editedWorkgroup.setName(workgroup.getName());
+        return workgroupService.save(editedWorkgroup);
+    }
+
+    @RequestMapping(value = "/api/workgroups/{workgroupId}", method = RequestMethod.DELETE, produces="application/json")
+    @ResponseBody
+    public void removeWorkgroup(@PathVariable Long workgroupId) {
+        authorizer.isAdmin();
+
+        Workgroup workgroup = workgroupService.findOneById(workgroupId);
+
+        workgroupService.delete(workgroupId);
     }
 }
