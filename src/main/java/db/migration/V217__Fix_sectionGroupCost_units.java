22a95b2465385f35da151b0359bb37f67fb91d45
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2018-12-04 13:24:05 -0800

    Fill in sectionGroupCosts that dont have units set

---
+++ src/main/java/db/migration/V217__Fix_sectionGroupCost_units.java
@@ -0,0 +1,63 @@
+package db.migration;
+
+    import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
+
+    import java.sql.Connection;
+    import java.sql.Date;
+    import java.sql.PreparedStatement;
+    import java.sql.ResultSet;
+    import java.sql.Time;
+    import java.util.ArrayList;
+    import java.util.HashMap;
+    import java.util.List;
+    import java.util.Map;
+
+public class V217__Fix_sectionGroupCost_units implements JdbcMigration {
+
+    /**
+     * Fixes sectionGroupCosts that were in-correctly created without units
+     * @param connection
+     * @throws Exception
+     */
+    @Override
+    public void migrate(Connection connection) throws Exception {
+        PreparedStatement psSectionGroupCosts = connection.prepareStatement("SELECT * FROM SectionGroupCosts WHERE UnitsHigh IS NULL AND UnitsLow IS NULL;");
+        connection.setAutoCommit(false);
+
+        ResultSet rsSectionGroupCosts = psSectionGroupCosts.executeQuery();
+
+        while(rsSectionGroupCosts.next()) {
+            Long sectionGroupCostId = rsSectionGroupCosts.getLong("Id");
+            String subjectCode = rsSectionGroupCosts.getString("SubjectCode");
+            String courseNumber = rsSectionGroupCosts.getString("CourseNumber");
+            String effectiveTermCode = rsSectionGroupCosts.getString("EffectiveTermCode");
+
+            PreparedStatement psCourse = connection.prepareStatement("SELECT * FROM Courses c WHERE c.SubjectCode = ? AND c.CourseNumber = ? AND c.EffectiveTermCode = ?;");
+            psCourse.setString(1, subjectCode);
+            psCourse.setString(2, courseNumber);
+            psCourse.setString(3, effectiveTermCode);
+
+            ResultSet rsCourse = psCourse.executeQuery();
+            if (rsCourse.first()) {
+                Float unitsHigh = rsCourse.getFloat("UnitsHigh");
+                Float unitsLow = rsCourse.getFloat("UnitsLow");
+
+                PreparedStatement psUpdateSectionGroupCost = connection.prepareStatement(
+                    " UPDATE SectionGroupCosts" +
+                        " SET UnitsHigh = ?," +
+                        " UnitsLow = ?" +
+                        " WHERE Id = ?;"
+                );
+
+                psUpdateSectionGroupCost.setLong(1, sectionGroupCostId);
+                psUpdateSectionGroupCost.setFloat(2, unitsHigh);
+                psUpdateSectionGroupCost.setFloat(3, unitsLow);
+                psUpdateSectionGroupCost.execute();
+                psUpdateSectionGroupCost.close();
+            }
+        }
+
+        // Commit changes
+        connection.commit();
+    }
+}
