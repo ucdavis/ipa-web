addc4e01032b6901f883f9fe46037a566862d8ef
Jarold Wong <cwjwong@ucdavis.edu>
2019-06-27 16:53:59 -0700

    Add create teachingCallComment endpoint

--- src/main/java/edu/ucdavis/dss/ipa/api/components/assignment/AssignmentViewTeachingCallReceiptController.java
+++ src/main/java/edu/ucdavis/dss/ipa/api/components/assignment/AssignmentViewTeachingCallReceiptController.java
@@ -1,11 +1,16 @@
 package edu.ucdavis.dss.ipa.api.components.assignment;
 
-import edu.ucdavis.dss.ipa.entities.*;
+import edu.ucdavis.dss.ipa.entities.TeachingCallComment;
+import edu.ucdavis.dss.ipa.entities.TeachingCallReceipt;
+import edu.ucdavis.dss.ipa.entities.Workgroup;
 import edu.ucdavis.dss.ipa.security.Authorizer;
-import edu.ucdavis.dss.ipa.services.*;
+import edu.ucdavis.dss.ipa.services.TeachingCallCommentService;
+import edu.ucdavis.dss.ipa.services.TeachingCallReceiptService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by Lloyd on 8/10/16.
@@ -13,9 +18,10 @@ import javax.inject.Inject;
 @RestController
 public class AssignmentViewTeachingCallReceiptController {
     @Inject TeachingCallReceiptService teachingCallReceiptService;
+    @Inject TeachingCallCommentService teachingCallCommentService;
     @Inject Authorizer authorizer;
-    
-    @RequestMapping(value = "/api/assignmentView/teachingCallReceipts/{teachingCallReceiptId}", method = RequestMethod.PUT, produces="application/json")
+
+    @RequestMapping(value = "/api/assignmentView/teachingCallReceipts/{teachingCallReceiptId}", method = RequestMethod.PUT, produces = "application/json")
     @ResponseBody
     public TeachingCallReceipt updateTeachingCallReceipt(@PathVariable long teachingCallReceiptId, @RequestBody TeachingCallReceipt teachingCallReceipt) {
         TeachingCallReceipt originalTeachingCallReceipt = teachingCallReceiptService.findOneById(teachingCallReceiptId);
@@ -28,4 +34,32 @@ public class AssignmentViewTeachingCallReceiptController {
         return teachingCallReceiptService.save(originalTeachingCallReceipt);
     }
 
+    @RequestMapping(value = "/api/assignmentView/teachingCallReceipts/{teachingCallReceiptId}/teachingCallComments", method = RequestMethod.POST, produces = "application/json")
+    @ResponseBody
+    public TeachingCallComment createTeachingCallComment(@PathVariable long teachingCallReceiptId,
+                                                         @RequestBody TeachingCallComment teachingCallCommentDTO,
+                                                         HttpServletResponse httpResponse) {
+        // Ensure valid params
+        TeachingCallReceipt teachingCallReceipt = teachingCallReceiptService.findOneById(teachingCallReceiptId);
+
+        if (teachingCallReceipt == null) {
+            httpResponse.setStatus(HttpStatus.NOT_FOUND.value());
+            return null;
+        }
+
+        // Authorization check
+        Long workGroupId = teachingCallReceipt.getSchedule().getWorkgroup().getId();
+        authorizer.hasWorkgroupRoles(workGroupId, "academicPlanner", "instructor");
+
+        teachingCallCommentDTO.setTeachingCallReceipt(teachingCallReceipt);
+
+        TeachingCallComment teachingCallComment = teachingCallCommentService.create(teachingCallCommentDTO);
+
+        if (teachingCallComment == null) {
+            httpResponse.setStatus(HttpStatus.NOT_FOUND.value());
+            return null;
+        }
+
+        return teachingCallComment;
+    }
 }
