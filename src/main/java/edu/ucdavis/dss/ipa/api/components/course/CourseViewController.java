897246924a21216cfe7617f5dc183d1ae0190605
Jarold Wong <cwjwong@ucdavis.edu>
2019-07-29 13:18:06 -0700

    Force create first section on sectionGroup creation

--- src/main/java/edu/ucdavis/dss/ipa/api/components/course/CourseViewController.java
+++ src/main/java/edu/ucdavis/dss/ipa/api/components/course/CourseViewController.java
@@ -110,6 +110,19 @@ public class CourseViewController {
 		SectionGroup newSectionGroup = sectionGroupService.findOrCreateByCourseIdAndTermCode(course.getId(), sectionGroup.getTermCode());
 		newSectionGroup.setPlannedSeats(sectionGroup.getPlannedSeats());
 
+		// Force create first section on sectionGroup creation
+		String sequenceNumber = null;
+		Character firstChar = null;
+
+		firstChar = course.getSequencePattern().charAt(0);
+		if (Character.isLetter(firstChar)) {
+			sequenceNumber = firstChar + "01";
+		} else {
+			sequenceNumber = course.getSequencePattern();
+		}
+
+		sectionService.findOrCreateBySectionGroupAndSequenceNumber(newSectionGroup, sequenceNumber);
+
 		return sectionGroupService.save(newSectionGroup);
 	}
 
