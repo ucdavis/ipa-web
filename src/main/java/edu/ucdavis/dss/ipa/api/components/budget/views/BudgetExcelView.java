f41be2d49f2d4d8a2a782cb71d15d6a98e55394a
Jarold Wong <cwjwong@ucdavis.edu>
2020-06-30 11:53:13 -0700

    filling out instructor salaries sheet

--- src/main/java/edu/ucdavis/dss/ipa/api/components/budget/views/BudgetExcelView.java
+++ src/main/java/edu/ucdavis/dss/ipa/api/components/budget/views/BudgetExcelView.java
@@ -3,8 +3,15 @@ package edu.ucdavis.dss.ipa.api.components.budget.views;
 import edu.ucdavis.dss.ipa.api.components.budget.views.factories.BudgetViewFactory;
 import edu.ucdavis.dss.ipa.entities.Budget;
 import edu.ucdavis.dss.ipa.entities.BudgetScenario;
+import edu.ucdavis.dss.ipa.entities.Instructor;
+import edu.ucdavis.dss.ipa.entities.InstructorCost;
+import edu.ucdavis.dss.ipa.entities.InstructorTypeCost;
+import edu.ucdavis.dss.ipa.entities.User;
+import edu.ucdavis.dss.ipa.entities.UserRole;
 import edu.ucdavis.dss.ipa.services.BudgetScenarioService;
 import edu.ucdavis.dss.ipa.services.BudgetService;
+import edu.ucdavis.dss.ipa.services.UserRoleService;
+import java.util.Optional;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -22,6 +29,7 @@ public class BudgetExcelView extends AbstractXlsView {
     @Inject BudgetViewFactory budgetViewFactory;
     @Inject BudgetService budgetService;
     @Inject BudgetScenarioService budgetScenarioService;
+    @Inject UserRoleService userRoleService;
     private List<BudgetView> budgetViews = null;
     public BudgetExcelView ( List<BudgetView> budgetViews ) {
         this.budgetViews = budgetViews;
@@ -37,11 +45,36 @@ public class BudgetExcelView extends AbstractXlsView {
 
         Sheet sheet = workbook.createSheet("Budget Summary");
         setExcelHeader(sheet, Arrays.asList("1", "2"));
-        for(BudgetView budgetView : budgetViews){
+
+        Sheet instructorSalariesSheet = workbook.createSheet("Instructor Salaries");
+        setExcelHeader(instructorSalariesSheet, Arrays.asList("Instructor", "Type", "Cost"));
+
+        for (BudgetView budgetView : budgetViews) {
             System.err.println("Instructor count in excel is " + budgetView.getActiveInstructors().size());
-            Row excelData = sheet.createRow(1);
-            excelData.createCell(0).setCellValue(budgetView.getActiveInstructors().size());
+
+            for (int i = 0; i < budgetView.getActiveInstructors().size(); i++) {
+                Row instructorSalaryRow = instructorSalariesSheet.createRow(i + 1);
+
+                Instructor instructor = budgetView.getActiveInstructors().get(i);
+                InstructorCost instructorCost = budgetView.getInstructorCosts().stream().filter(ic -> ic.getInstructor().getId() == instructor.getId()).findFirst().orElse(null);
+
+                String instructorName = instructor.getLastName() + " " + instructor.getFirstName();
+                instructorSalaryRow.createCell(0).setCellValue(instructorName);
+
+                User user = budgetView.users.stream().filter(u -> u.getLoginId().equals(instructor.getLoginId())).findFirst().orElse(null);
+                UserRole userRole = user.getUserRoles().stream().filter(ur -> ur.getRole().getId() == 15).findFirst().orElse(null);
+
+                instructorSalaryRow.createCell(1).setCellValue(userRole.getInstructorType().getDescription());
+
+                if (instructorCost != null) {
+                    instructorSalaryRow.createCell(2).setCellValue(String.valueOf(instructorCost.getCost()));
+                }
+            }
         }
+
+        instructorSalariesSheet.autoSizeColumn(0);
+        instructorSalariesSheet.autoSizeColumn(1);
+        instructorSalariesSheet.autoSizeColumn(2);
     }
 
     private void setExcelHeader(Sheet sheet, List<String> headers) {
