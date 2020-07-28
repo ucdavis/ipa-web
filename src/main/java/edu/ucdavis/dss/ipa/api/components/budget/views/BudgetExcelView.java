0c488dd5b3af2ef4cadc72d1fc0c7b8928587faf
Edgar Duran-Perez <eduranperez@ucdavis.edu>
2020-07-28 13:57:56 -0700

    rtt-24: set size on notes

--- src/main/java/edu/ucdavis/dss/ipa/api/components/budget/views/BudgetExcelView.java
+++ src/main/java/edu/ucdavis/dss/ipa/api/components/budget/views/BudgetExcelView.java
@@ -233,8 +233,9 @@ public class BudgetExcelView extends AbstractXlsxView {
         workbook = ExcelHelper.expandHeaders(workbook);
         workbook = ExcelHelper.ignoreErrors(workbook, Arrays.asList(IgnoredErrorType.NUMBER_STORED_AS_TEXT));
 
-        // Override expanding the comments column on the funds tab
-        // This is because it can be very large
+        // Override expanding the comments and notes columns on the funds tab
+        // This is because it can they can both be quite large
+        fundsSheet.setColumnWidth(4, 2560);
         fundsSheet.setColumnWidth(5, 2560);
     }
 
