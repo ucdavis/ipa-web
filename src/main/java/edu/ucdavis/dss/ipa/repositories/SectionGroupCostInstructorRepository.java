ee76be7d8d6119ccda61a75a35d48e5f33b2bcf6
Edgar Duran-Perez <eduranperez@ucdavis.edu>
2020-08-18 11:59:32 -0700

    fix class name

--- src/main/java/edu/ucdavis/dss/ipa/repositories/SectionGroupCostInstructorRepository.java
+++ src/main/java/edu/ucdavis/dss/ipa/repositories/SectionGroupCostInstructorRepository.java
@@ -3,6 +3,6 @@ package edu.ucdavis.dss.ipa.repositories;
 import edu.ucdavis.dss.ipa.entities.SectionGroupCostInstructor;
 import org.springframework.data.repository.CrudRepository;
 
-public interface SectionGroupCostInstructorsRepository extends CrudRepository<SectionGroupCostInstructor, Long> {
+public interface SectionGroupCostInstructorRepository extends CrudRepository<SectionGroupCostInstructor, Long> {
 
 }
