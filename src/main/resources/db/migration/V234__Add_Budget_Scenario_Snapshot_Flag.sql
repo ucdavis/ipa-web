ae2fe1409a23fe24d8df1e45d41d1522797c2274
Jarold Wong <cwjwong@ucdavis.edu>
2020-08-21 16:43:51 -0700

    Lengthen scenario name column

--- src/main/resources/db/migration/V234__Add_Budget_Scenario_Snapshot_Flag.sql
+++ src/main/resources/db/migration/V234__Add_Budget_Scenario_Snapshot_Flag.sql
@@ -1,12 +1,11 @@
 ALTER TABLE `BudgetScenarios`
     ADD COLUMN `isSnapshot` TINYINT(1) NOT NULL,
     ADD COLUMN `TaCost` FLOAT NULL,
-    ADD COLUMN `ReaderCost` FLOAT NULL;
+    ADD COLUMN `ReaderCost` FLOAT NULL,
+    MODIFY `Name` VARCHAR(100);
 
 ALTER TABLE `InstructorCosts`
-    ADD COLUMN `BudgetScenarioId` INT(11) NULL,
-    MODIFY `BudgetId` INT(11) NULL;
+    ADD COLUMN `BudgetScenarioId` INT(11) NULL;
 
 ALTER TABLE `InstructorTypeCosts`
-    ADD COLUMN `BudgetScenarioId` INT(11) NULL,
-    MODIFY `BudgetId` INT(11) NULL;
+    ADD COLUMN `BudgetScenarioId` INT(11) NULL;
