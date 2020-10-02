11bf925b6d9cfacf0d3d0f37822511200cd2157a
Edgar Duran-Perez <eduranperez@ucdavis.edu>
2020-10-02 11:57:11 -0700

    dont abbreviate

--- src/main/resources/db/migration/V236__Add_LineItemCategory_FundsFromDO.sql
+++ src/main/resources/db/migration/V236__Add_LineItemCategory_FundsFromDO.sql
@@ -1,1 +1,1 @@
-INSERT INTO `LineItemCategories` (`Description`) VALUES ('Additional Funds from DO');
+INSERT INTO `LineItemCategories` (`Description`) VALUES ("Additional Funds From Dean's Office");
