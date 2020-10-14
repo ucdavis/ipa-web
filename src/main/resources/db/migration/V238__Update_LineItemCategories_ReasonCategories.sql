866dbbfa8fd0058378f934c8743bc0ed1b757784
Jarold Wong <cwjwong@ucdavis.edu>
2020-10-14 15:17:02 -0700

    add backticks

--- src/main/resources/db/migration/V238__Update_LineItemCategories_ReasonCategories.sql
+++ src/main/resources/db/migration/V238__Update_LineItemCategories_ReasonCategories.sql
@@ -1,13 +1,13 @@
 -- tracking previous manual updates in migration
-UPDATE `LineItemCategories` SET Description = "Funds From Dean's Office" WHERE Id = 1;
-UPDATE `LineItemCategories` SET Description = "Internal Buyout" WHERE Id = 2;
-UPDATE `LineItemCategories` SET Description = "Range Adjustment Funds" WHERE Id = 4;
+UPDATE `LineItemCategories` SET `Description` = "Funds From Dean's Office" WHERE `Id` = 1;
+UPDATE `LineItemCategories` SET `Description` = "Internal Buyout" WHERE `Id` = 2;
+UPDATE `LineItemCategories` SET `Description` = "Range Adjustment Funds" WHERE `Id` = 4;
 
-DELETE FROM `LineItemCategories` WHERE id >= 7;
+DELETE FROM `LineItemCategories` WHERE `Id` >= 7;
 INSERT INTO `LineItemCategories` (`Id`, `Description`) VALUES (7, "External Buyout");
 INSERT INTO `LineItemCategories` (`Id`, `Description`) VALUES (8, "Additional Funds From Dean's Office");
 INSERT INTO `LineItemCategories` (`Id`, `Description`) VALUES (9, "Funds not in GENT Account");
 
 -- remove unused entries
-DELETE FROM `ReasonCategories` WHERE id < 10;
-UPDATE `SectionGroupCosts` SET `ReasonCategoryId` = NULL WHERE ReasonCategoryId < 10;
+DELETE FROM `ReasonCategories` WHERE `Id` < 10;
+UPDATE `SectionGroupCosts` SET `ReasonCategoryId` = NULL WHERE `ReasonCategoryId` < 10;
