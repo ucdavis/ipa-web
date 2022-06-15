d1802ac6749a9221c31f85594d5db9b2209aff6a
Jarold Wong <cwjwong@ucdavis.edu>
2022-06-15 13:35:06 -0700

    add blanks after each group of course number

--- src/main/java/edu/ucdavis/dss/ipa/api/components/scheduleSummaryReport/views/ScheduleSummaryReportAnnualExcelView.java
+++ src/main/java/edu/ucdavis/dss/ipa/api/components/scheduleSummaryReport/views/ScheduleSummaryReportAnnualExcelView.java
@@ -4,14 +4,11 @@ import edu.ucdavis.dss.ipa.entities.SectionGroup;
 import edu.ucdavis.dss.ipa.entities.Term;
 import edu.ucdavis.dss.ipa.entities.enums.TermDescription;
 import edu.ucdavis.dss.ipa.utilities.ExcelHelper;
-import java.time.Instant;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -32,7 +29,6 @@ public class ScheduleSummaryReportAnnualExcelView extends AbstractXlsxView {
     protected void buildExcelDocument(Map<String, Object> model, Workbook workbook, HttpServletRequest request,
                                       HttpServletResponse response) throws Exception {
         final long year = scheduleSummaryReportView.getYear();
-        String dateOfDownload = new Date().toString();
         String fileName = "Annual Schedule";
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
         LocalDateTime now = LocalDateTime.now();
@@ -63,13 +59,31 @@ public class ScheduleSummaryReportAnnualExcelView extends AbstractXlsxView {
                     Comparator.comparing(sg -> sg.getCourse().getCourseNumber())).collect(Collectors.toList());
 
             int currentRow = 0;
-            for (SectionGroup sectionGroup : termSectionGroups) {
+            for (int i = 0; i < termSectionGroups.size(); i++) {
+                SectionGroup currentSectionGroup = termSectionGroups.get(i);
                 List<Object> rowValues = new ArrayList<>(Arrays.asList(
-                    sectionGroup.getCourse().getShortDescription(),
-                    sectionGroup.getTeachingAssignments().size() > 0 ?
-                        sectionGroup.getTeachingAssignments().get(0).getInstructorDisplayName() : null,
-                    sectionGroup.getPlannedSeats()
+                    currentSectionGroup.getCourse().getShortDescription(),
+                    currentSectionGroup.getTeachingAssignments().size() > 0 ?
+                        currentSectionGroup.getTeachingAssignments().get(0).getInstructorDisplayName() : null,
+                    currentSectionGroup.getPlannedSeats()
                 ));
+
+                // add filler if previous course number was different
+                if (i > 0 && currentSectionGroup.getCourse().getCourseNumber()
+                    .compareTo(termSectionGroups.get(i - 1).getCourse().getCourseNumber()) != 0) {
+
+                    int spaces = rowValues.size();
+
+                    if (completedTerm > 0) {
+                        List<Object> row = dataRows.get(currentRow);
+                        fillRow(row, "", spaces);
+                        currentRow++;
+                    } else {
+                        List<Object> emptyRow = new ArrayList<>();
+                        fillRow(emptyRow, "", spaces);
+                        dataRows.add(new ArrayList<>(emptyRow));
+                    }
+                }
 
                 if (completedTerm > 0) {
                     // start another column
@@ -78,16 +92,35 @@ public class ScheduleSummaryReportAnnualExcelView extends AbstractXlsxView {
                         dataRows.get(currentRow).addAll(rowValues);
                     } else {
                         // prepend row with fill spaces
-                        int fillSpaces = completedTerm * rowValues.size();
-                        for (int i = 0; i < fillSpaces; i++) {
-                            rowValues.add(0, "");
-                        }
+                        int spaces = completedTerm * rowValues.size();
+                        fillRow(rowValues, "", spaces, 0);
 
                         dataRows.add(new ArrayList<>(rowValues));
                     }
                 } else {
                     dataRows.add(new ArrayList<>(rowValues));
                 }
+
+//                // add blank row if next course number is different
+//                if (i + 1 < termSectionGroups.size() && sectionGroup.getCourse().getCourseNumber()
+//                    .compareTo(termSectionGroups.get(i + 1).getCourse().getCourseNumber()) != 0) {
+//                    if (completedTerm > 0) {
+//                        // append to existing row
+//                        int fillSpaces = rowValues.size();
+//                        List<Object> nextRow = dataRows.get(currentRow + 1);
+//                        for (int i = 0; i < fillSpaces; i++) {
+//                            nextRow.add("");
+//                        }
+//                        currentRow++;
+//                    } else {
+//                        int fillSpaces = rowValues.size();
+//                        List<Object> emptyRow = new ArrayList<>();
+//                        for (int i = 0; i < fillSpaces; i++) {
+//                            emptyRow.add("");
+//                        }
+//                        dataRows.add(new ArrayList<>(emptyRow));
+//                    }
+//                }
                 currentRow++;
             }
             completedTerm++;
@@ -101,5 +134,19 @@ public class ScheduleSummaryReportAnnualExcelView extends AbstractXlsxView {
         }
 
         ExcelHelper.expandHeaders(workbook);
+    }
+
+    private List<Object> fillRow(List<Object> row, Object value, int length) {
+        for (int i = 0; i < length; i++) {
+            row.add(value);
+        }
+        return row;
+    }
+
+    private List<Object> fillRow(List<Object> row, Object value, int length, int start) {
+        for (int i = 0; i < length; i++) {
+            row.add(start, value);
+        }
+        return row;
     }
 }
