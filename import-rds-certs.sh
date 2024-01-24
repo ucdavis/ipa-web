6a729a602ffb9604fa5820ebb00abd60152e9078
Jarold Wong <cwjwong@ucdavis.edu>
2024-01-24 14:17:04 -0800

    update to multi-stage build

--- import-rds-certs.sh
+++ import-rds-certs.sh
@@ -4,11 +4,11 @@ then
 mkdir -p "${mydir}"
 fi
 
-curl -sS "https://truststore.pki.rds.amazonaws.com/us-west-2/us-west-2-bundle.pem" > ${mydir}/us-west-2-bundle.pem
+wget -O ${mydir}/us-west-2-bundle.pem "https://truststore.pki.rds.amazonaws.com/us-west-2/us-west-2-bundle.pem"
 awk 'split_after == 1 {n++;split_after=0} /-----END CERTIFICATE-----/ {split_after=1}{print > "rds-ca-" n ".pem"}' < ${mydir}/us-west-2-bundle.pem
 
 for CERT in rds-ca-*; do
-  alias=$(openssl x509 -noout -text -in $CERT | perl -ne 'next unless /Subject:/; s/.*(CN=|CN = )//; print')
+  alias=$(openssl x509 -noout -text -in $CERT | awk '/Subject:/ {sub(/.*CN *= */, ""); print; exit}')
   echo "Importing $alias"
   keytool -import -file ${CERT} -alias "${alias}" -storepass changeit -keystore "$JAVA_HOME/jre/lib/security/cacerts" -noprompt
   rm $CERT
